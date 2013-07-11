b45b4381fd189b99188aa784ff001044bd003587
auk262 <auk262@psu.edu>
2013-07-11 11:28:06 -0400

    changes to Printencodetire1 and Printexpttable

--- src/edu/psu/compbio/seqcode/projects/akshay/multigpstesting/Printencodetire1.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/multigpstesting/Printencodetire1.java
@@ -1,5 +1,16 @@
 package edu.psu.compbio.seqcode.projects.akshay.multigpstesting;
 
+import java.io.*;
+import java.sql.*;
+import edu.psu.compbio.seqcode.projects.akshay.query.*;
+
 public class Printencodetire1 {
 
+	public static void main(String[] args) throws SQLException, IOException{
+		Pullencodetire1 getlist = new Pullencodetire1();
+		Scanner sc = new Scanner("/Users/akshaykakumanu/Desktop/tt");
+		getlist.getFactorAliases(sc.getMap());
+		getlist.getExperiments();
+		getlist.printExptMap();
+	}
 }
--- src/edu/psu/compbio/seqcode/projects/akshay/query/Printexpttable.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/query/Printexpttable.java
@@ -1,5 +1,20 @@
 package edu.psu.compbio.seqcode.projects.akshay.query;
 
-public class Printexpttable {
+import java.io.*;
+import java.util.*;
+import java.sql.*;
+import edu.psu.compbio.seqcode.gse.utils.*;
 
+public class Printexpttable {
+	
+	public static void main(String[] args) throws IOException, SQLException, NotFoundException{
+		Scanner sc = new Scanner("/Users/akshaykakumanu/Desktop/temp");
+		Map<String, String[]> map = sc.getMap();
+		for(String[] command : map.values()){
+			Pullexpttable gexp = new Pullexpttable(command);
+			gexp.executeSQLExptCommand();
+			gexp.getTableFromRS();
+			gexp.printTable();
+		}
+	}
 }
