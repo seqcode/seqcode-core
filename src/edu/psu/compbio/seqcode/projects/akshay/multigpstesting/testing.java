c463e967ed8d52c9ea1ec0a06cbfc1fdb21f8a4f
auk262 <auk262@psu.edu>
2013-07-17 14:01:41 -0400

    more testing

--- src/edu/psu/compbio/seqcode/projects/akshay/multigpstesting/testing.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/multigpstesting/testing.java
@@ -3,7 +3,7 @@ package edu.psu.compbio.seqcode.projects.akshay.multigpstesting;
 public class testing {
 	
 	public static void main(String[] args){
-		System.out.println("akshay");
+		System.out.println("akshay psu");
 	}
 
 }
