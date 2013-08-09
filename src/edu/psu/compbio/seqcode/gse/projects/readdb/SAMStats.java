aca9562104dd6e5e86cc6e51d0baa6cf2745ff63
Shaun Mahony <mahony@psu.edu>
2013-08-09 13:31:18 -0400

    Added methods to print read length and total read count in a SAM/BAM

--- src/edu/psu/compbio/seqcode/gse/projects/readdb/SAMStats.java
+++ src/edu/psu/compbio/seqcode/gse/projects/readdb/SAMStats.java
@@ -19,11 +19,12 @@ import edu.psu.compbio.seqcode.gse.utils.RealValuedHistogram;
 
 public class SAMStats {
 
-	private Double totalHits=0.0, LHits=0.0, RHits=0.0; 
+	private Double totalReads = 0.0, totalHits=0.0, LHits=0.0, RHits=0.0; 
 	private Double singleEnd=0.0, properPair=0.0, pairMapped=0.0, notPrimary=0.0;
 	private Double junctions=0.0;
 	private Double uniquelyMapped=0.0;
 	private Double weight=0.0;
+	private Integer readLen =-1;
 	private Double pairedEndSameChr=0.0, pairedEndDiffChr=0.0;
 	private Boolean bowtie1=false, bowtie2=false;
 	private RealValuedHistogram histo;
@@ -32,6 +33,8 @@ public class SAMStats {
         Options options = new Options();
         options.addOption("i","insdist",false,"print insert size distribution");
         options.addOption("s","stats",false,"print mapping stats");
+        options.addOption("l","readlen",false,"read length");
+        options.addOption("c","readcount",false,"read count");
         options.addOption("bt2",false,"input is from bowtie2");
         options.addOption("bt1",false,"input is from bowtie1 (tested for --best --strata -m 1)");
         CommandLineParser parser = new GnuParser();
@@ -42,6 +45,10 @@ public class SAMStats {
         	s.printInsertDistrib();
         if(cl.hasOption("stats"))
         	s.printStats();
+        if(cl.hasOption("readlen"))
+        	s.printReadLength();
+        if(cl.hasOption("readcount"))
+        	s.printReadCount();
     }
 	
 	public SAMStats(boolean bowtie1, boolean bowtie2){
@@ -53,6 +60,8 @@ public class SAMStats {
         CloseableIterator<SAMRecord> iter = reader.iterator();
         while (iter.hasNext()) {
             SAMRecord record = iter.next();
+            if(readLen==-1)
+            	readLen = record.getReadLength();
             if(bowtie1)
             	processBT1SAMRecord(record);
             else if(bowtie2)
@@ -65,6 +74,7 @@ public class SAMStats {
     }
 	
 	public void processSAMRecord(SAMRecord r){
+		totalReads++;
 		if(!r.getReadUnmappedFlag()){
 			totalHits++;
 			int count = 1;  //Have to figure out something for BWA when reporting multiple alignments
@@ -115,6 +125,7 @@ public class SAMStats {
 	}
 	
 	public void processBT1SAMRecord(SAMRecord r){
+		totalReads++;
 		if(!r.getReadUnmappedFlag())
 			if(r.getIntegerAttribute("XM")!=null){
 				int xm = r.getIntegerAttribute("XM");
@@ -172,6 +183,7 @@ public class SAMStats {
 	}
 	
 	public void processBT2SAMRecord(SAMRecord r){
+		totalReads++;
 		if(!r.getReadUnmappedFlag()){
 			totalHits++;
 			int count =1; //TODO: Fix this if using bowtie2 for multi-mapping reads
@@ -227,6 +239,13 @@ public class SAMStats {
 		}
 	}
 	
+	public void printReadLength(){
+		System.out.println(readLen);
+	}
+	
+	public void printReadCount(){
+		System.out.println(String.format("%.0f",+totalReads));
+	}
 	
 	public void printInsertDistrib(){
 		histo.printContents();
