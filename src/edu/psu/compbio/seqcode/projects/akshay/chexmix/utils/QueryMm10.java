f6b27385f883a4958373db1b36a294feb08f1505
auk262 <auk262@psu.edu>
2013-08-15 16:51:05 -0400

    debugging

---
+++ src/edu/psu/compbio/seqcode/projects/akshay/chexmix/utils/QueryMm10.java
@@ -0,0 +1,21 @@
+package edu.psu.compbio.seqcode.projects.akshay.chexmix.utils;
+
+public class QueryMm10 extends QueryGenome{
+
+	public QueryMm10(String chr, int midpoint, int range) {
+		super(chr, midpoint, range);
+	}
+
+	@Override
+	public void fillGenomePath() {
+		this.genomepath =  "/gpfs/home/auk262/group/genomes/mm9";
+		
+	}
+
+	@Override
+	public void fillGenomePath(String path) {
+		this.genomepath = path;
+		
+	}
+
+}
