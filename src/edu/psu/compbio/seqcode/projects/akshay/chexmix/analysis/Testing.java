e36901f46b22c469bebc6af999cc6d408055658c
auk262 <auk262@psu.edu>
2013-09-13 11:47:13 -0400

    testing

--- src/edu/psu/compbio/seqcode/projects/akshay/chexmix/analysis/Testing.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/chexmix/analysis/Testing.java
@@ -21,7 +21,8 @@ public class Testing {
 			BindingLocation bl =  new BindingLocation(151128370,"chr1",c);
 			bl.filltags(loadTags);
 			List<Integer> temp = bl.getConcatenatedTags(bl.vecpos.midpoint, bl.vecpos.range, bl.vecpos.orientation);
-			for(int i : temp){
+			
+			for(int i=0; i<temp.size();i++){
 				System.out.println(i+"\t"+temp.get(i));
 			}
 		}
