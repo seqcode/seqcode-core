347970ca221ed2b6c7eaf97be67047a0598219fa
auk262 <auk262@psu.edu>
2013-09-14 14:10:59 -0400

    debug

--- src/edu/psu/compbio/seqcode/projects/akshay/chexmix/analysis/BuildSeed.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/chexmix/analysis/BuildSeed.java
@@ -364,7 +364,7 @@ public class BuildSeed {
 			
 			//debug line
 			System.out.println("added node");
-			for(int k=0; k< workingTree.get(19).composite.length; k++){
+			for(int k=0; k< workingTree.get(18).composite.length; k++){
 				System.out.println(k+"\t"+workingTree.get(18).composite[k]);
 			}
 		} while(max_pcc>conf.getSeedCutoff() && workingTree.size()>0);
