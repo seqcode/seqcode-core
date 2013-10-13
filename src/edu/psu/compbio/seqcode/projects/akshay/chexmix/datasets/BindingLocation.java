d51f9ac74e9621c3cc1c0d002ea821a1c5e773b0
auk262 <auk262@psu.edu>
2013-10-13 13:45:08 -0400

    dubug lines commented

--- src/edu/psu/compbio/seqcode/projects/akshay/chexmix/datasets/BindingLocation.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/chexmix/datasets/BindingLocation.java
@@ -110,7 +110,7 @@ public class BindingLocation {
 	public void filltags(LoadTags loader){
 		QueryTags tagsfetcher = new QueryTags(this.midpoint,this.range, this.chr, this.Smoothsize);
 		//debug line
-		System.out.println(this.getName());
+		//System.out.println(this.getName());
 		//end
 		this.vecpos = tagsfetcher.getTags(loader, "+");
 		this.vecneg = tagsfetcher.getTags(loader, "-");
