d1068c5ffae078af47383860615b412bbddfb78b
auk262 <auk262@psu.edu>
2013-10-13 13:07:08 -0400

    debug

--- src/edu/psu/compbio/seqcode/projects/akshay/chexmix/utils/QueryTags.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/chexmix/utils/QueryTags.java
@@ -34,6 +34,7 @@ public class QueryTags {
 				//debug line
 				System.out.println(start_ind);
 				System.out.println(tempStarts.length);
+				System.out.println(tempStarts[tempStarts.length]);
 				//end
 				if( start_ind < 0 ) { start_ind = -start_ind - 1; }
 				for(int k=this.midpoint-this.range; k<this.midpoint+this.range; k++){
