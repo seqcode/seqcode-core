24c363a835281f5a2f4d26829e27aa9f0a68ab00
ikaka89 <akshay.kakumanu@gmail.com>
2014-01-14 23:22:40 -0500

    Building Bayesments code

--- src/edu/psu/compbio/seqcode/projects/akshay/bayesments/analysis/Bayesments.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/bayesments/analysis/Bayesments.java
@@ -8,32 +8,41 @@ import edu.psu.compbio.seqcode.projects.akshay.bayesments.framework.Config;
 public class Bayesments {
 	
 	public static void run(String[] args){
+		
 		Config c = new Config(args);
-		c.makeGPSOutputDirs(c.doEMplot());
+		if(c.helpWanter()){
+			System.err.println("MultiGPS:");
+			System.err.println(c.getArgsList());
+		}else{
+			c.makeGPSOutputDirs(c.doEMplot());
+			System.out.println("Loading Reads\n");
+			ExperimentManager manager = new ExperimentManager(c,c.loadReads());
+			System.out.println("Filling Training Dara\n");
+			GenomicLocations trainingData = new GenomicLocations(manager,c);
+			
+			System.out.println("Initializing EM\n");
+			EMtrain EM = new EMtrain(c,trainingData);
+			
+			System.out.println("Running EM\n");
+			EM.runEM();
 		
-		ExperimentManager manager = new ExperimentManager(c,c.loadReads());
-		GenomicLocations trainingData = new GenomicLocations(manager,c);
+			System.out.println("PI-C values\n");
+			System.out.println(EM.getPIj());
 		
-		EMtrain EM = new EMtrain(c,trainingData);
-		EM.runEM();
+			System.out.println("MU-C values\n");
+			System.out.println(EM.getMUc());
 		
-		System.out.println("PI-C values\n");
-		System.out.println(EM.getPIj());
+			System.out.println("MU-F values\n");
+			System.out.println(EM.getMUf());
 		
-		System.out.println("MU-C values\n");
-		System.out.println(EM.getMUc());
+			System.out.println("SIGMA-C values\n");
+			System.out.println(EM.getSIGMAc());
 		
-		System.out.println("MU-F values\n");
-		System.out.println(EM.getMUf());
+			System.out.println("SIGMA-F values\n");
+			System.out.println(EM.getSIGMAf());
 		
-		System.out.println("SIGMA-C values\n");
-		System.out.println(EM.getSIGMAc());
-		
-		System.out.println("SIGMA-F values\n");
-		System.out.println(EM.getSIGMAf());
-		
-		System.out.println("Bjk values\n");
-		System.out.println(EM.getBjk());
-	}
-
+			System.out.println("Bjk values\n");
+			System.out.println(EM.getBjk());
+		}
+	}	
 }
