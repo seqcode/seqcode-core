a0c274b8ba1cb9ac3ea0ce75b91b68cfddeba12f
auk262 <auk262@psu.edu>
2014-05-11 15:09:35 -0400

    Added new code to KmerMapper class

--- src/edu/psu/compbio/seqcode/projects/akshay/Utils/KmerMapper.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/Utils/KmerMapper.java
@@ -6,6 +6,8 @@ import java.util.List;
 import edu.psu.compbio.seqcode.gse.datasets.general.Point;
 import edu.psu.compbio.seqcode.gse.datasets.general.Region;
 import edu.psu.compbio.seqcode.gse.datasets.general.ScoredStrandedRegion;
+import edu.psu.compbio.seqcode.gse.datasets.general.StrandedPoint;
+import edu.psu.compbio.seqcode.gse.datasets.general.StrandedRegion;
 import edu.psu.compbio.seqcode.gse.datasets.motifs.WeightMatrix;
 import edu.psu.compbio.seqcode.gse.datasets.species.Genome;
 import edu.psu.compbio.seqcode.gse.ewok.verbs.SequenceGenerator;
@@ -16,8 +18,8 @@ import edu.psu.compbio.seqcode.projects.shaun.Utilities;
 
 public class KmerMapper {
 	
-	public List<Point> points;
-	public List<Region> regions;
+	public List<StrandedPoint> points;
+	public List<StrandedRegion> regions;
 	public int k;
 	public SequenceGenerator<Region> seqgen = new SequenceGenerator<Region>();
 	public int[][][] MapMatrix;
@@ -35,51 +37,50 @@ public class KmerMapper {
 	
 	//Settors
 	
-	public void setRegions(boolean shift, String peaksFileName,String SeqPathFile){
+	public void setRegions(String peaksFileName,String SeqPathFile){
 		List<Point> tempPoints = Utilities.loadPeaksFromPeakFile(gen, peaksFileName, winSize);
 		List<Region> tempRegions = Utilities.loadRegionsFromPeakFile(gen, peaksFileName, winSize);
-		if(shift){
+	
+		seqgen.useCache(true);
+		seqgen.setGenomePath(SeqPathFile);
+		scorer = new WeightMatrixScorer(motif,seqgen);
 			
-			seqgen.useCache(true);
-			seqgen.setGenomePath(SeqPathFile);
-			scorer = new WeightMatrixScorer(motif,seqgen);
-			
-			for(int i=0 ; i<tempRegions.size(); i++){
-				
-				Point p= tempPoints.get(i);
-				Region r = tempRegions.get(i);
-				WeightMatrixScoreProfile profiler = scorer.execute(r);
-				int bestMotifIndex = profiler.getMaxIndex();
-				double bestMotifScore = profiler.getMaxScore(bestMotifIndex);
-				if(bestMotifScore >= motifThres){
-					int closestDist = Integer.MAX_VALUE;
-					int closestIndex = -1;
-					
-					for(int z=0; z<r.getWidth()-motif.length()+1; z++){
-						double currScore= profiler.getMaxScore(z);
-						if(currScore>=motifThres){
-							int motifCenterCoord = z+(motif.length()/2)+r.getStart();
-							int dist = Math.abs(p.getLocation() - motifCenterCoord);
-							if(dist<closestDist){
-								closestDist = dist;
-								closestIndex = z;
-							}
+		for(int i=0 ; i<tempRegions.size(); i++){
+			Point p= tempPoints.get(i);
+			Region r = tempRegions.get(i);
+			WeightMatrixScoreProfile profiler = scorer.execute(r);
+			int bestMotifIndex = profiler.getMaxIndex();
+			double bestMotifScore = profiler.getMaxScore(bestMotifIndex);
+			if(bestMotifScore >= motifThres){
+				int closestDist = Integer.MAX_VALUE;
+				int closestIndex = -1;
+				char closestStrand = '\0';
+				for(int z=0; z<r.getWidth()-motif.length()+1; z++){
+					double currScore= profiler.getMaxScore(z);
+					if(currScore>=motifThres){
+						int motifCenterCoord = z+(motif.length()/2)+r.getStart();
+						int dist = Math.abs(p.getLocation() - motifCenterCoord);
+						if(dist<closestDist){
+							closestDist = dist;
+							closestIndex = z;
+							closestStrand = profiler.getMaxStrand(z);
 						}
 					}
-					
-					Region hitreg = new Region(gen,r.getChrom(), r.getStart()+closestIndex, r.getStart()+closestIndex+motif.length()-1);
-					points.add(new Point(gen,hitreg.getChrom(),hitreg.getStart()));
+				}
+				
+				if(closestStrand == '+'){
+					Point addP = new Point(gen,r.getChrom(),r.getStart()+closestIndex);
+					points.add(new StrandedPoint(addP,closestStrand));
+				}else{
+					Point addP = new Point(gen,r.getChrom(),r.getStart()+closestIndex+motif.length()-1);
+					points.add(new StrandedPoint(addP,closestStrand));
 				}
 			}
-			for(Point p : points){
-				regions.add(p.expand(winSize/2));
+			for(StrandedPoint sp : points){
+				regions.add(new StrandedRegion(sp.expand(winSize/2),sp.getStrand()));
 			}
 			
-		}else{
-			points = tempPoints;
-			regions = tempRegions;
 		}
-		
 	}
 	
 	
@@ -93,17 +94,14 @@ public class KmerMapper {
 			String seq = seqgen.execute(regions.get(i)).toUpperCase();
 			if(seq.contains("N"))
 				continue;
+			String seqOriented = (regions.get(i).getStrand() == '+') ? seq : SequenceUtils.reverseComplement(seq);
 			for(int j=0; j<(seq.length()-k+1); j++){
-				String currK = seq.substring(j, j+k);
+				String currK = seqOriented.substring(j, j+k);
 				String revCurrK = SequenceUtils.reverseComplement(currK);
 				int currKInt = Utilities.seq2int(currK);
 				int revCurrKInt = Utilities.seq2int(revCurrK);
 				int kmer = currKInt < revCurrKInt ? currKInt : revCurrKInt;
-				boolean reverseComplement = currKInt > revCurrKInt;
-				if(reverseComplement)
-					MapMatrix[kmer][i][j+k-1] = 1;
-				else
-					MapMatrix[kmer][i][j]=1;
+				MapMatrix[kmer][i][j] = 1;
 			}
 		}
 		
@@ -117,12 +115,12 @@ public class KmerMapper {
 			int kmerID = Utilities.seq2int(kmerSet[i]);
 			int[][] kmerMap = MapMatrix[kmerID];
 			boolean pass = false;
-			for(int j=0; j<kmerMap[0].length; j++){
-				int colSum =0;
-				for(int k=0; k<kmerMap.length; k++){
-					colSum = colSum + kmerMap[j][k];
+			for(int j=0; j<kmerMap[0].length; j++){ // over all positions
+				int rowSum =0;
+				for(int k=0; k<kmerMap.length; k++){ // over all peaks
+					rowSum = rowSum + kmerMap[j][k];
 				}
-				if((int)((colSum*100)/kmerMap.length) > percCutoff){
+				if((int)((rowSum*100)/kmerMap.length) > percCutoff){
 					pass = true;
 				}
 			}
@@ -137,11 +135,11 @@ public class KmerMapper {
 			int[][] kmerMap = MapMatrix[i];
 			boolean pass = false;
 			for(int j=0; j<kmerMap[0].length; j++){
-				int colSum =0;
+				int rowSum =0;
 				for(int k=0; k<kmerMap.length; k++){
-					colSum = colSum + kmerMap[j][k];
+					rowSum = rowSum + kmerMap[j][k];
 				}
-				if((int)((colSum*100)/kmerMap.length) > percCutoff){
+				if((int)((rowSum*100)/kmerMap.length) > percCutoff){
 					pass = true;
 				}
 			}
@@ -152,6 +150,93 @@ public class KmerMapper {
 		}
 	}
 	
+	public void printInformativeKmerWithinDistance(int percCutoff, int distance){
+		for(int i=0; i<MapMatrix.length; i++){
+			int[][] kmerMap = MapMatrix[i];
+			boolean pass = false;
+			int midP = (int)(kmerMap[0].length/2);
+			for(int j=(midP-distance); j<(midP+distance); j++){ // over all locations
+				int rowSum =0;
+				for(int k=0; k<kmerMap.length; k++){ // over all peaks
+					rowSum = rowSum + kmerMap[j][k];
+				}
+				if((int)((rowSum*100)/kmerMap.length) > percCutoff){
+					pass = true;
+				}
+			}
+			if(pass){
+				System.out.println(Utilities.int2seq(i, k));
+			}
+		}
+		
+	}
+	
+	public void printInformativeKmerWithinDistanceFromSet(String[] kmerSet, int percCutoff, int distance){
+		for(int i=0; i<kmerSet.length; i++){
+			int kmerID = Utilities.seq2int(kmerSet[i]);
+			int[][] kmerMap = MapMatrix[kmerID];
+			boolean pass = false;
+			int midP = (int)(kmerMap[0].length/2);
+			for(int j=(midP-distance); j<(midP+distance); j++){ // over all locations
+				int rowSum =0;
+				for(int k=0; k<kmerMap.length; k++){ // over all peaks
+					rowSum = rowSum + kmerMap[j][k];
+				}
+				if((int)((rowSum*100)/kmerMap.length) > percCutoff){
+					pass = true;
+				}
+			}
+			if(pass){
+				System.out.println(Utilities.int2seq(i, k));
+			}
+		}
+	}
+	
+	/**
+	 * excludes k-mers coming from motifs that have been centered
+	 * @param percCutoff
+	 * @param distance
+	 */
+	public void printInformativeKmerFlanks(int percCutoff, int distance){
+		for(int i=0; i<MapMatrix.length; i++){
+			int[][] kmerMap = MapMatrix[i];
+			boolean pass = false;
+			int midP = (int)(kmerMap[0].length/2);
+			for(int j=(midP-distance); j<(midP+distance); j++){ // over all locations
+				int rowSum =0;
+				for(int k=0; k<kmerMap.length; k++){ // over all peaks
+					rowSum = rowSum + kmerMap[j][k];
+				}
+				if((int)((rowSum*100)/kmerMap.length) > percCutoff && (j<midP || j> (midP+motif.length()-k))){
+					pass = true;
+				}
+			}
+			if(pass){
+				System.out.println(Utilities.int2seq(i, k));
+			}
+		}
+	}
+	
+	public void printInformativeKmerFlanksFromSet(String[] kmerSet, int percCutoff, int distance){
+		for(int i=0; i<kmerSet.length; i++){
+			int kmerID = Utilities.seq2int(kmerSet[i]);
+			int[][] kmerMap = MapMatrix[kmerID];
+			boolean pass = false;
+			int midP = (int)(kmerMap[0].length/2);
+			for(int j=(midP-distance); j<(midP+distance); j++){ // over all locations
+				int rowSum =0;
+				for(int k=0; k<kmerMap.length; k++){ // over all peaks
+					rowSum = rowSum + kmerMap[j][k];
+				}
+				if((int)((rowSum*100)/kmerMap.length) > percCutoff && (j<midP || j> (midP+motif.length()-k))){
+					pass = true;
+				}
+			}
+			if(pass){
+				System.out.println(Utilities.int2seq(i, k));
+			}
+		}
+	}
 	
 	public static void main(String[] args){
 		
