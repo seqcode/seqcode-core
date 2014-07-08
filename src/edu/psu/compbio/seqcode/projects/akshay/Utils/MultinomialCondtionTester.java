f6593384ea4de4e5ebe7871a26d0636172f7254a
auk262 <auk262@psu.edu>
2014-07-08 10:54:35 -0400

    finished coding multinomial class

--- src/edu/psu/compbio/seqcode/projects/akshay/Utils/MultinomialCondtionTester.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/Utils/MultinomialCondtionTester.java
@@ -1,6 +1,10 @@
 package edu.psu.compbio.seqcode.projects.akshay.Utils;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import umontreal.iro.lecuyer.probdistmulti.MultinomialDist;
@@ -11,9 +15,13 @@ import edu.psu.compbio.seqcode.deepseq.experiments.ExperimentCondition;
 import edu.psu.compbio.seqcode.deepseq.experiments.ExperimentManager;
 import edu.psu.compbio.seqcode.deepseq.experiments.ExperimentSet;
 import edu.psu.compbio.seqcode.gse.datasets.general.Point;
+import edu.psu.compbio.seqcode.gse.tools.utils.Args;
+import edu.psu.compbio.seqcode.gse.utils.io.parsing.GFFEntry;
+import edu.psu.compbio.seqcode.gse.utils.io.parsing.ParseGFF;
 
 import edu.psu.compbio.seqcode.projects.multigps.framework.Config;
 import edu.psu.compbio.seqcode.projects.multigps.features.BindingEvent;
+import edu.psu.compbio.seqcode.projects.sequtils.PointsToEvents;
 
 public class MultinomialCondtionTester {
 	
@@ -45,7 +53,7 @@ public class MultinomialCondtionTester {
 			for(ExperimentCondition ec : exptSet.getConditions()){
 				double repSigScaleCount=0;
 				for(ControlledExperiment cr : ec.getReplicates()){
-					repSigScaleCount = cr.getSignal().getHitCount()/cr.getControlScaling();
+					repSigScaleCount = cf.getRepSigHits(cr)/cr.getControlScaling();
 					condSigScaledCounts[ec.getIndex()] += repSigScaleCount;
 				}
 			}
@@ -108,6 +116,43 @@ public class MultinomialCondtionTester {
 		config.setScalingSlidingWindow(50000);
 		
 		ExperimentManager manager = new ExperimentManager(config);
+		ExperimentSet eset = manager.getExperimentSet();
+		System.err.println("Conditions:\t"+eset.getConditions().size());
+		for(ExperimentCondition c : eset.getConditions()){
+			System.err.println("Condition "+c.getName()+":\t#Replicates:\t"+c.getReplicates().size());
+		}
+		String siteFile = Args.parseString(args, "gff", null);
+		if(siteFile==null){
+			 System.exit(1);
+		}
+		int win = Args.parseInteger(args, "win", 50);
+		double minFold = Args.parseDouble(args, "minfold", 2);
+		
+		List<BindingEvent> features = new ArrayList<BindingEvent>();
+		List<Point> Sites = new ArrayList<Point>();
+		HashMap<Point,GFFEntry> siteToGFFEntry = new HashMap<Point, GFFEntry>();
+		
+		try{
+			File gffFile = new File(siteFile);
+			ParseGFF parser = new ParseGFF(gffFile);
+			while(parser.hasNext()){
+				GFFEntry site = parser.next();
+				Point currPt = new Point(config.getGenome(), site.getChr(), site.getMidPoint());
+				Sites.add(currPt);
+				siteToGFFEntry.put(currPt, site);
+			}
+			
+		} catch(IOException e){
+			//silent exception
+		}
+		
+		//Convert our points to events
+		PointsToEvents p2e = new PointsToEvents(config, manager, Sites, win,true);
+		features = p2e.execute();
+		
+		
+		MultinomialCondtionTester tester = new MultinomialCondtionTester(config, eset, features, minFold, config.getMappableGenomeLength());
+		tester.execute();
 		
 		
 	}
