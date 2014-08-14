01bacd24a61d0537a36c4bef6d8967570587ae3f
auk262 <auk262@psu.edu>
2014-08-14 15:14:46 -0400

    Added extra utilities to PairedEndSandBox

--- src/edu/psu/compbio/seqcode/projects/akshay/utils/PairEndStatSandbox.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/utils/PairEndStatSandbox.java
@@ -3,81 +3,49 @@ package edu.psu.compbio.seqcode.projects.akshay.utils;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 import edu.psu.compbio.seqcode.genome.Genome;
+import edu.psu.compbio.seqcode.genome.Genome.ChromosomeInfo;
+import edu.psu.compbio.seqcode.genome.location.NamedRegion;
 import edu.psu.compbio.seqcode.gse.datasets.seqdata.SeqAlignment;
 import edu.psu.compbio.seqcode.gse.datasets.seqdata.SeqDataLoader;
 import edu.psu.compbio.seqcode.gse.datasets.seqdata.SeqHitPair;
+import edu.psu.compbio.seqcode.genome.location.Region;
 
 import edu.psu.compbio.seqcode.gse.datasets.seqdata.SeqLocator;
 import edu.psu.compbio.seqcode.gse.gsebricks.verbs.chipseq.SeqExpander;
+import edu.psu.compbio.seqcode.gse.gsebricks.verbs.location.ChromRegionIterator;
 import edu.psu.compbio.seqcode.gse.tools.utils.Args;
+import edu.psu.compbio.seqcode.gse.utils.ArgParser;
 import edu.psu.compbio.seqcode.gse.utils.NotFoundException;
+import edu.psu.compbio.seqcode.projects.shaun.Utilities;
 
 public class PairEndStatSandbox {
-	
-	// Current this class uses the old seqexpander methods instead of the new hitloaders. ..
-	// Change to the new hitloader methods as soon as possible
-	
 	private SeqLocator locator;
-	
-	private LinkedList<SeqAlignment> alignments;
-	private Genome lastGenome;
-	private SeqDataLoader loader;
-	private boolean closeLoader;
+	private SeqExpander expander;
+	private boolean cleanExpander;
 	private Genome gen;
 	
 	//stats
+	//public double[] fragment_size;
 	
-	public double[] fragment_size; 
 	
-	public PairEndStatSandbox(SeqLocator expt, Genome g) throws SQLException, IOException {
-		this.locator = expt;
-		this.loader = new SeqDataLoader();
-		closeLoader = true;
+	public PairEndStatSandbox(SeqLocator sloc, Genome g) throws SQLException, IOException {
+		this.locator = sloc;
+		this.expander = new SeqExpander(sloc);
+		this.cleanExpander = true;
 		this.gen = g;
-		try {
-			this.getAligns(g);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}	
+		
 	}
-	private void getAligns(Genome genome) throws SQLException {
-		if (alignments != null && genome.equals(lastGenome)) {
-            return;
-        }
-        alignments = new LinkedList<SeqAlignment>();
-        lastGenome = genome;
-        try {
-            alignments.addAll(locator.loadAlignments(loader, genome));
-        } catch (SQLException e) {
-            e.printStackTrace(System.err);
-        } catch (NotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-	
-	
 	
 	// Fetchers
 	
-	public List<SeqHitPair> loadHitsbyChrom(String chr){
-		List<SeqHitPair> ret = new ArrayList<SeqHitPair>();
-		int chrID = gen.getChromID(chr);
-		for(SeqAlignment a : this.alignments){
-			try {
-				List<SeqHitPair> temp = loader.loadPairsByChrom(a, chrID);
-				ret.addAll(temp);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
+	public Iterator<SeqHitPair> loadHitsbyChrom(NamedRegion chrom){
+		Iterator<SeqHitPair> ret = this.expander.getPairs(chrom);
 		return ret;
-		
 	}
 	
 	
@@ -87,16 +55,21 @@ public class PairEndStatSandbox {
 	
 	//Stats Calculators
 	
-	public void setFragmentsSizes(){
-		this.fragment_size = new double[500];
-		for(String chr : this.gen.getChromList()){
-			if(!chr.contains("random")){
-				List<SeqHitPair> tmp = this.loadHitsbyChrom(chr);
-				for(SeqHitPair pair : tmp){
+	public void printFragmentsSizes(){
+		double[] fragment_size = new double[500];
+		
+		ChromRegionIterator chrItr = new ChromRegionIterator(this.gen);
+		
+		while(chrItr.hasNext()){
+			NamedRegion chrom = chrItr.next();
+			if(!chrom.getName().contains("random")){
+				Iterator<SeqHitPair> pairItr = this.loadHitsbyChrom(chrom);
+				while(pairItr.hasNext()){
+					SeqHitPair pair = pairItr.next();
 					if(pair.getCode() == 1 && pair.getMidpoint() != null){
 						int dist  = pair.getLeft().getFivePrime() > pair.getRight().getFivePrime() ? pair.getLeft().getFivePrime() - pair.getRight().getFivePrime() : pair.getRight().getFivePrime() - pair.getLeft().getFivePrime();
 						if(dist <=500){
-							this.fragment_size[dist-1]++;
+							fragment_size[dist-1]++;
 						}
 					}
 				
@@ -105,50 +78,84 @@ public class PairEndStatSandbox {
 		}
 		
 		double total = 0;
-		for(int i=0; i<this.fragment_size.length; i++){
-			total = total+ this.fragment_size[i];
+		for(int i=0; i<fragment_size.length; i++){
+			total = total+ fragment_size[i];
 		}
-		for(int i=0; i<this.fragment_size.length; i++){
-			this.fragment_size[i] = this.fragment_size[i]/total;
+		for(int i=0; i<fragment_size.length; i++){
+			fragment_size[i] = fragment_size[i]/total;
+		}
+		
+		for(int i=0; i<fragment_size.length; i++){
+			System.out.println(Integer.toString(i+1)+"\t"+Double.toString(fragment_size[i]));
+		}
+		
+		
+	}
+	
+	// make sure win is -1 if you pass a regions file instead of peaks file
+	public void printWindowCounts(String peaks_file, int win, int minFragLen, int maxFragLen){
+		List<Region> regs  = Utilities.loadRegionsFromPeakFile(gen, peaks_file, win);
+		for(Region r : regs){
+			int count = 0;
+			Iterator<SeqHitPair> pairItr = this.expander.getPairs(r);
+			while(pairItr.hasNext()){
+				SeqHitPair pair = pairItr.next();
+				if(pair.getCode() == 1 && pair.getMidpoint() == null){
+					int fragLen = pair.getRight().getFivePrime() - pair.getLeft().getFivePrime()+1;
+					if(fragLen >= minFragLen && fragLen < maxFragLen){
+						count++;
+					}
+				}
+			}
+			System.out.println(r.getLocationString()+"\t"+Integer.toString(count));
 		}
 	}
 	
 	
 	
 	
-	// getters
-	
-	public double[] getFragmentSizes(){return this.fragment_size;}
 	
 	
-	
-	public void close() {
-        if (closeLoader) {
-            loader.close();
-        }
-        loader = null;
-        if (alignments != null) {
-            alignments.clear();
-        }
-    }
+	public void clean(){
+		if(isNotClean()){
+			this.expander.close();
+			this.cleanExpander = false;
+		}
+	}
 
 
-    public boolean isClosed() {
-        return loader == null;
+    public boolean isNotClean() {
+        return this.cleanExpander;
     }
 	
 	
 	public static void main(String[] args) throws NotFoundException, SQLException, IOException{
+		ArgParser ap = new ArgParser(args);
 		SeqLocator expt = Args.parseSeqExpt(args, "expt").get(0);
 		Genome g = Args.parseGenome(args).cdr();
 		PairEndStatSandbox analyzer = new PairEndStatSandbox(expt, g);
-		analyzer.setFragmentsSizes();
-		double[] frags = analyzer.getFragmentSizes();
-		analyzer.close();
-		for(int i=0; i<frags.length; i++){
-			System.out.println(Integer.toString(i+1)+"\t"+Double.toString(frags[i]));
-		}
 		
+		if(ap.hasKey("fragSize")){
+			analyzer.printFragmentsSizes();
+			analyzer.clean();
+		}else if(ap.hasKey("windowCount")){
+			String peaks_file;
+			int win;
+			if(ap.hasKey("peaks")){
+				peaks_file = ap.getKeyValue("peaks");
+				win = Args.parseInteger(args, "win", -1);
+			}else{
+				peaks_file = ap.getKeyValue("region");
+				win = -1;
+			}
+			
+			int minFragLen = Args.parseInteger(args, "minFrag", 140);
+			int maxFragLen = Args.parseInteger(args, "maxFrag", 200);
+			
+			analyzer.printWindowCounts(peaks_file, win, minFragLen, maxFragLen);
+			analyzer.clean();
+			
+		}
 		
 	}
 }
