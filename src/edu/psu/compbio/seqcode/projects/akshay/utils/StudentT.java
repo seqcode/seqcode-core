c5345fffa2aaab79fbe743bfef953033c2d6ff45
auk262 <auk262@psu.edu>
2014-11-12 10:55:09 -0500

    removing bugs in StudentT class

--- src/edu/psu/compbio/seqcode/projects/akshay/utils/StudentT.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/utils/StudentT.java
@@ -16,6 +16,7 @@ public class StudentT {
 	public double[] vec2;
 	public double pval;
 	public double Tstat;
+	public double v;
 	
 	public StudentT(double[] vec1, double[] vec2) {
 		this.vec1=vec1;
@@ -27,20 +28,20 @@ public class StudentT {
 		double std1 = StatUtil.std(vec1);
 		double std2 = StatUtil.std(vec2);
 		
-		double Tstat_num = Math.pow(mean1, 2.0) - Math.pow(mean2, 2.0);
+		double Tstat_num = mean1- mean2;
 		double Tstat_den = Math.sqrt((Math.pow(std1, 2.0)/vec1.length)+(Math.pow(std2, 2.0)/vec2.length));
 		Tstat = Tstat_num/Tstat_den;
 		
 		double v_num =  Math.pow((Math.pow(std1, 2.0)/vec1.length)+(Math.pow(std2, 2.0)/vec2.length),2);
 		double v_den = (Math.pow((Math.pow(std1, 2.0)/vec1.length),2.0)/(vec1.length-1))+(Math.pow((Math.pow(std2, 2.0)/vec2.length),2.0)/(vec2.length-1));
 		
-		double v = v_num/v_den;
+		v = v_num/v_den;
 		pval = StatUtil.studentTPvalue(Tstat, v);
 	}
 	
 	public double getPval(){return this.pval;}
 	public double getTstat(){return this.Tstat;}
-	
+	public double getV(){return this.v;}
 	
 	public static void main(String[] args){
 		try{
@@ -91,7 +92,7 @@ public class StudentT {
 				StudentT tester = new StudentT(x,y);
 				System.out.println(tester.getPval());
 				System.out.println(tester.getTstat());
-				
+				System.out.println(tester.getV());
 			}
 			
 			
