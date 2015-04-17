71d4186e47022e3624b8b8280d556f5de88119a1
auk262 <auk262@psu.edu>
2015-04-16 22:41:36 -0400

    finished motifstokmer class

--- src/edu/psu/compbio/seqcode/projects/akshay/utils/MotifToKmers.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/utils/MotifToKmers.java
@@ -2,6 +2,11 @@ package edu.psu.compbio.seqcode.projects.akshay.utils;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -11,9 +16,11 @@ import edu.psu.compbio.seqcode.genome.location.Region;
 import edu.psu.compbio.seqcode.genome.location.RepeatMaskedRegion;
 import edu.psu.compbio.seqcode.gse.datasets.motifs.WeightMatrix;
 import edu.psu.compbio.seqcode.gse.gsebricks.verbs.sequence.SequenceGenerator;
+import edu.psu.compbio.seqcode.gse.utils.ArgParser;
 import edu.psu.compbio.seqcode.gse.utils.io.RegionFileUtilities;
 import edu.psu.compbio.seqcode.gse.utils.sequence.SequenceUtils;
 import edu.psu.compbio.seqcode.gse.gsebricks.verbs.location.RepeatMaskedGenerator;
+import edu.psu.compbio.seqcode.projects.shaun.MotifAnalysisSandbox;
 
 public class MotifToKmers {
 	
@@ -25,7 +32,8 @@ public class MotifToKmers {
 	private int Kmin;
 	private int Kmax;
 	private WeightMatrix motif;
-	private double[] threshholds;
+	//private double[] threshholds;
+	private int threshlevel;
 	private String posOutFile;
 	private String negOutFile;
 	
@@ -33,12 +41,84 @@ public class MotifToKmers {
 	
 	
 	
-	public MotifToKmers() {
-		// TODO Auto-generated constructor stub
+	public MotifToKmers (GenomeConfig gconf) {
+		gcon = gcon;
+	}
+	
+	
+	//Mutators
+	public void setPosSet(List<Region> pos){posSet = pos;}
+	public void setNegSet(List<Region> neg){negSet = neg;}
+	public void setKmin(int kmin){Kmin=kmin;}
+	public void setKmax(int kmax){Kmax = kmax;}
+	public void setMontif(WeightMatrix matrix){motif = matrix;}
+	public void  setThreslevel(int t){threshlevel = t;}
+	public void setPosOut(String po){posOutFile = po;}
+	public void setNegOut(String no){posOutFile = no;}
+	
+	
+	public static void main(String[] args) throws IOException, ParseException{
+		ArgParser ap = new ArgParser(args);
+		GenomeConfig gcon = new GenomeConfig(args);
+		MotifToKmers runner = new MotifToKmers(gcon);
+		boolean cache = ap.hasKey("seq");
+		String seqpath="";
+		if(cache){
+			seqpath = ap.getKeyValue("seq");
+		}
+		int win = ap.hasKey("win") ? new Integer(ap.getKeyValue("win")).intValue():-1;
+		String peaksFile = ap.getKeyValue("peaks");
+		List<Point> peaksP = RegionFileUtilities.loadPeaksFromPeakFile(gcon.getGenome(), ap.getKeyValue("peaksP"), win);
+		List<Region> regsP = new ArrayList<Region>();
+		for(Point p : peaksP){
+			regsP.add(p.expand(win/2));
+		}
+		
+		
+		List<Point> peaksN = RegionFileUtilities.loadPeaksFromPeakFile(gcon.getGenome(), ap.getKeyValue("peaksN"), win);
+		List<Region> regsN = new ArrayList<Region>();
+		for(Point p : peaksN){
+			regsN.add(p.expand(win/2));
+		}
+		
+		String outname = ap.getKeyValue("out");
+		String posout = outname.concat("-pos.kmers");
+		String negout = outname.concat("-neg.kmers");
+		
+		int thresLvl = ap.hasKey("threslevel") ? new Integer(ap.getKeyValue("threslevel")).intValue():10;
+		
+		int kmin = ap.hasKey("kmin") ? new Integer(ap.getKeyValue("kmin")).intValue():3;
+		int kmax = ap.hasKey("kmax") ? new Integer(ap.getKeyValue("kmax")).intValue():8;
+		
+		String motiffile = ap.getKeyValue("motiffile");
+		String backfile = ap.getKeyValue("back");
+		
+		List<WeightMatrix> matrixList = MotifAnalysisSandbox.loadMotifFromFile(motiffile, backfile, gcon.getGenome());
+		WeightMatrix matrix = matrixList.get(0);
+		
+		runner.setPosSet(regsP);
+		runner.setNegSet(regsN);
+		runner.setKmin(kmin);
+		runner.setKmax(kmax);
+		runner.setMontif(matrix);
+		runner.setThreslevel(thresLvl);
+		runner.setPosOut(posout);
+		runner.setNegOut(negout);
+		
+		runner.execute(cache, seqpath);
+		
+		
 	}
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	@SuppressWarnings("unchecked")
 	public void execute(boolean useCache, String genPath) throws IOException{
 		SequenceGenerator seqgen = new SequenceGenerator();
 		seqgen.useCache(useCache);
@@ -54,28 +134,53 @@ public class MotifToKmers {
 		StringBuilder negSB = new StringBuilder();
 		
 		headerSB.append("Region");
+		
+		HashMap<String, List<Integer>> kmerCountsP = new HashMap<String,List<Integer>>();
+		HashMap<String, List<Integer>> kmerCountsN = new HashMap<String,List<Integer>>();
+		List<String> kmer_order=new ArrayList<String>();
 		for(int k=Kmin; k<=Kmax; k++){ // iterating over different k-mer lengths
 			int numK = (int)Math.pow(4, k);
-			int[] kmerCounts = new int[numK];
+			int[] currKKmerCountsP = new int[numK];
+			int[] currKKmerCountsN = new int[numK];
 			boolean[] isMotifKmer = new boolean[numK];
 			
-			for(int i=0; i<numK; i++){ //iterating over k-mers of a given length
+			List<Double> scores = new ArrayList<Double>();
+			for(int i=0; i<numK; i++){ //iterating over k-mers of a given length and finding motifs that belong to a motif
 				String currKmer = RegionFileUtilities.int2seq(i, k);
 				String revCurrKmer = SequenceUtils.reverseComplement(currKmer);
-				int revKmerIndex = RegionFileUtilities.seq2int(revCurrKmer);
 				double scoref = scoreKmer(currKmer);
 				double scoreR = scoreKmer(revCurrKmer);
-				
 				double score = scoref > scoreR ? scoref : scoreR;
-				if(score >= threshholds[k-Kmin] && i < revKmerIndex){
+				scores.add(score);
+			}
+			@SuppressWarnings("rawtypes")
+			Comparator cmp = Collections.reverseOrder();  
+			Collections.sort(scores,cmp);
+			
+			double threshold = scores.get((int)(threshlevel*scores.size()/100));
+			
+			for(int i=0; i<numK; i++){
+				String currKmer = RegionFileUtilities.int2seq(i, k);
+				String revCurrKmer = SequenceUtils.reverseComplement(currKmer);
+				int revCurrKmerInd = RegionFileUtilities.seq2int(revCurrKmer);
+				double scoref = scoreKmer(currKmer);
+				double scoreR = scoreKmer(revCurrKmer);
+				double score = scoref > scoreR ? scoref : scoreR;
+				
+				if(score >threshold && i < revCurrKmerInd  ){
 					isMotifKmer[i] = true;
 				}
+				
 			}
 			
-			for(Region r : posSet){
-				posSB.append(r.getLocationString());
+			
+			for(Region r : posSet){ // Count kmer frequencies at the positive set
 				for(int i=0; i<numK; i++){
-					kmerCounts[i] = 0;
+					currKKmerCountsP[i] = 0;
+				}
+				
+				if(!kmerCountsP.containsKey(r.getLocationString())){
+					kmerCountsP.put(r.getLocationString(),new ArrayList<Integer>());
 				}
 				String seq = seqgen.execute(r).toUpperCase();
 				if(seq.contains("N"))
@@ -85,31 +190,86 @@ public class MotifToKmers {
 					String currK = seq.substring(i, i+k);
 					String revCurrK = SequenceUtils.reverseComplement(currK);
 					if(isMotifKmer[RegionFileUtilities.seq2int(currK)]){
-						kmerCounts[RegionFileUtilities.seq2int(currK)]++;
+						currKKmerCountsP[RegionFileUtilities.seq2int(currK)]++;
 					}
 					if(isMotifKmer[RegionFileUtilities.seq2int(revCurrK)]){
-						kmerCounts[RegionFileUtilities.seq2int(revCurrK)]++;
+						currKKmerCountsP[RegionFileUtilities.seq2int(revCurrK)]++;
 					}
 				}
 			}
 			
-			for(int i=0; i<numK; i++){
-				headerSB.append("\t");
-				headerSB.append(RegionFileUtilities.int2seq(i, k));
+			for(Region r : negSet){
+				for(int i=0; i<numK; i++){
+					currKKmerCountsN[i] = 0;
+				}
+				
+				if(!kmerCountsP.containsKey(r.getLocationString())){
+					kmerCountsP.put(r.getLocationString(),new ArrayList<Integer>());
+				}
+				
+				String seq = seqgen.execute(r).toUpperCase();
+				if(seq.contains("N"))
+					continue;
+				
+				for(int i=0; i<(seq.length()-k+1); i++){
+					String currK = seq.substring(i, i+k);
+					String revCurrK = SequenceUtils.reverseComplement(currK);
+					if(isMotifKmer[RegionFileUtilities.seq2int(currK)]){
+						currKKmerCountsN[RegionFileUtilities.seq2int(currK)]++;
+					}
+					if(isMotifKmer[RegionFileUtilities.seq2int(revCurrK)]){
+						currKKmerCountsN[RegionFileUtilities.seq2int(revCurrK)]++;
+					}
+				}
 				
 			}
-				//foutP.write("#"+getProgramName()+"\n");
+			
+			for (Region r : posSet){
+				for(int i=0; i<numK; i++){
+					if(isMotifKmer[i]){
+						headerSB.append("\t");
+						headerSB.append(RegionFileUtilities.int2seq(i, k));
+						kmerCountsP.get(r.getLocationString()).add(currKKmerCountsP[i]);
+						kmer_order.add(RegionFileUtilities.int2seq(i, k));
+					}
+				}
+			}
+			
+			for(Region r : negSet){
+				for(int i=0; i<numK; i++){
+					if(isMotifKmer[i]){
+						kmerCountsN.get(r.getLocationString()).add(currKKmerCountsN[i]);
+					}
+				}
+			}
+			
+			for(String rlocation : kmerCountsP.keySet()){
+				posSB.append(rlocation);
+				for(int c : kmerCountsP.get(rlocation)){
+					posSB.append("\t");
+					posSB.append(c);
+				}
+				posSB.append("\n");
+			}
+			
+			for(String rlocation : kmerCountsN.keySet()){
+				negSB.append(rlocation);
+				for(int c : kmerCountsN.get(rlocation)){
+					negSB.append("\t");
+					negSB.append(c);
+				}
+				negSB.append("\n");
+			}
 			
 			
+			foutP.write(headerSB.toString()+"\n");
+			foutP.write(posSB.toString()+"\n");
+			foutP.close();
 			
-			
-			
-			
-			
-			
-			
+			foutN.write(headerSB.toString()+"\n");
+			foutN.write(negSB.toString()+"\n");
+			foutN.close();
 		}
-		
 	}
 	
 	
