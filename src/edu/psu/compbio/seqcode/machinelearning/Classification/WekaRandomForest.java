4fbeb37518bd38b29ee7b3a33965597da4b2b64b
ikaka89 <akshay.kakumanu@gmail.com>
2015-06-16 19:26:14 -0400

    Small bug fixes

--- src/edu/psu/compbio/seqcode/machinelearning/Classification/WekaRandomForest.java
+++ src/edu/psu/compbio/seqcode/machinelearning/Classification/WekaRandomForest.java
@@ -1,6 +1,6 @@
 package edu.psu.compbio.seqcode.machinelearning.Classification;
 
-import weka.classifiers.s;
+import weka.classifiers.*;
 import weka.classifiers.trees.*;
 
 public class WekaRandomForest {
