b994576c7644398c87ad5ddbd19b51351ee27dff
Naomi <nuy11@psu.edu>
2015-06-23 18:28:48 -0400

    1 ) fixed problems in CrossContaminationEstimator

--- src/edu/psu/compbio/seqcode/projects/naomi/CrossContaminationEstimator.java
+++ src/edu/psu/compbio/seqcode/projects/naomi/CrossContaminationEstimator.java
@@ -1,10 +1,12 @@
 package edu.psu.compbio.seqcode.projects.naomi;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 //import java.util.Collections;
 //import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import edu.psu.compbio.seqcode.deepseq.StrandedBaseCount;
 import edu.psu.compbio.seqcode.deepseq.experiments.ExperimentManager;
@@ -20,38 +22,52 @@ public class CrossContaminationEstimator {
 	
 	protected GenomeConfig gconfig;
 	protected ExptConfig econfig;
-//	protected ExperimentManager manager;
 	
-//	public CrossContaminationEstimator(GenomeConfig gcon, ExptConfig econ, ExperimentManager man){
 	public CrossContaminationEstimator(GenomeConfig gcon, ExptConfig econ){	
 		gconfig = gcon;
 		econfig = econ;
-//		manager = man;
 	}
 	
 	public void printDataPoints(){
 		
 		ExperimentManager manager = new ExperimentManager(econfig);
 
-		Genome genome = gconfig.getGenome();	
-//		List<String> chromNames = genome.getChromList();	
+		Genome genome = gconfig.getGenome();
+		List<String> chromNames = genome.getChromList();
+		int chromNum = chromNames.size();
+		int maxchromSize= 0;
+		for (String chrom : chromNames){
+			if (genome.getChromLength(chrom)< maxchromSize){
+				maxchromSize = genome.getChromLength(chrom);
+			}
+		}
 
 		int sampleSize = manager.getSamples().size();
 		
-		float[] tempCounts = new float [sampleSize];
+//		float [][] sampleCounts = new float [(int) genome.getGenomeLength()][sampleSize];
+//		for (int i = 0; i< genome.getGenomeLength(); i++){
+//			for (int j = 0; j < sampleSize; j++){
+//				sampleCounts[i][j] = 0;
+//			}
+//		}
 		
 		// this is the static array that can store all genome positions
-		float[] [] dataPoints = new float [(int) genome.getGenomeLength()] [3];
-		
-		//initialize dataPoints array
+		float[] [] dataPoints = new float [(int) genome.getGenomeLength()] [3];		
 		for (int i = 0; i< genome.getGenomeLength(); i ++){
 			for (int j = 0; j <3; j ++){
 				dataPoints[i][j] = 0;
 			}
 		}
 		
-		int dataPointsIndex = 0;
+		float[][] bpCounts = new float [maxchromSize][sampleSize];
+		for (int i = 0; i< maxchromSize;i++){
+			for (int j = 0; j<sampleSize;j ++){
+				bpCounts[i][j] = 0;
+			}
+		}
 		
+		int dataPointsIndex = 0;
+
 		Iterator<Region> chroms = new ChromosomeGenerator<Genome>().execute(genome);
 		//iterating each chromosome; each chromosome is a region.
 		while (chroms.hasNext()) {
@@ -59,55 +75,65 @@ public class CrossContaminationEstimator {
 			Region currChrom = chroms.next();
 			
 			//get base pair counts for each chromosome
-			List<StrandedBaseCount> bpcounts = new ArrayList<StrandedBaseCount>();
+			Map<Sample, List<StrandedBaseCount>> sampleCountsMap = new HashMap<Sample, List<StrandedBaseCount>>();
 			for (Sample sample : manager.getSamples()){
-				bpcounts = sample.getBases(currChrom); 
+				sampleCountsMap.put(sample,sample.getBases(currChrom)); 
+			}
+			
+			int sampleCounter=0;
+			
+			for (Sample sample : sampleCountsMap.keySet()){
+				List<StrandedBaseCount> currentCounts = sampleCountsMap.get(sample);
+				for (StrandedBaseCount hits: currentCounts){
+					bpCounts[hits.getCoordinate()][sampleCounter]=hits.getCount();	
+				}
+				sampleCounter++;
 			}
 			
 			int maxIndex = 0;
 			float maxcounts = 0;
 			float restSum = 0;
-
-			for (int i = 0; i < bpcounts.size(); i ++){
-				
-				for (int j = 0; sampleSize <0; j++){
-					tempCounts[j] = bpcounts.get(i).getCount();
-				}			
 			
-				for (int j = 0; sampleSize <0; j++){
-					if (tempCounts[j] > maxcounts){
-						maxcounts = tempCounts[j];
+			//for each position in the chromosome, find the max among samples and copy it to dataPoints
+			for (int i = 0; i<maxchromSize;i++){
+				// iterating one position among different samples
+				for (int j = 0; j<sampleSize;j++){
+					if (bpCounts[i][j]>maxcounts){
+						bpCounts[i][j] = maxcounts;
 						maxIndex = j;
+					}					
+				}
+				for (int j = 0; j<sampleSize;j++){
+					if (maxcounts != bpCounts[dataPointsIndex][j]){
+						restSum = restSum + bpCounts[i][j];
 					}
 				}
-				for (int j = 0; sampleSize <0; j++){
-					if (maxcounts != tempCounts[j]){
-						restSum = restSum + tempCounts[j];
-					}
-				}
-				if (maxcounts!= 0){
+				if(maxcounts!=0){
 					dataPoints[dataPointsIndex][0] = maxcounts;
 					dataPoints[dataPointsIndex][1] = restSum;
-					dataPoints[dataPointsIndex][2] = maxIndex;
+					dataPoints[dataPointsIndex][2] = maxIndex;	
 				}
-				dataPointsIndex++;
+				dataPointsIndex++;		
 				maxcounts = 0;
-				restSum = 0;		
-				
-				for (int j = 0; sampleSize<0;j++){
-					tempCounts[j]= 0;
+				restSum = 0;
+			}
+			//clearing values in bpCounts
+			for (int i = 0; i<maxchromSize; i++){
+				for (int j = 0; j <sampleSize; j++){
+					bpCounts[i][j] = 0;
 				}
 			}
-		}
-		
+			// remove all mapping 
+			sampleCountsMap.clear();		
+			}
+
 		//printing datapoints
 		int i = 0;
 		while (dataPoints [i][0] !=0){
 			System.out.println(dataPoints[i][0]+"\t"+dataPoints[i][1]+"\t"+dataPoints[i][2]);			
-			}
 			i ++;
-	}							
-	
+		}
+	}								
 	
 	public static void main(String[] args){
 		
