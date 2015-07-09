d9f433cb4a0c0a56478156bd01b425939c34e376
auk262 <auk262@psu.edu>
2015-07-09 10:59:00 -0400

    Added Co-occurrence features (kmer pairs) as a type of kmer-model to scan for high odds regions in a given list of seqs (KmerModelScanner)

--- src/edu/psu/compbio/seqcode/projects/akshay/utils/KmerModelScanner.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/utils/KmerModelScanner.java
@@ -22,6 +22,8 @@ public class KmerModelScanner {
 	
 	protected GenomeConfig gcon;
 	protected double[] kmerweights;
+	protected double[][] kmerpairwights;
+	protected boolean isPairModel = false;
 	protected List<Point> peaks;
 	protected List<Region> regions;
 	protected int k; // kmer length of the model
@@ -42,6 +44,9 @@ public class KmerModelScanner {
 	public void setK(int K){k=K;}
 	public void setminM(int m){minM=m;}
 	public void setmaxM(int m){maxM=m;}
+	public void setModelType(boolean isPair){isPair = isPairModel;}
+	public void setKmerPairWeights(double[][] pw){kmerpairwights = pw;}
+	
 	
 	public void printKmerMountains(boolean useCache, String genpath, double oddsThresh){
 		@SuppressWarnings("rawtypes")
@@ -68,6 +73,27 @@ public class KmerModelScanner {
 						int kmer = currKInt<revCurrKInt ? currKInt : revCurrKInt;
 						score = score+kmerweights[kmer];
 					}
+					if(isPairModel){
+						for(int j=0; j<(motif.length()-k+1-1); j++){
+							String currKP1 = motif.substring(j, j+k);
+							String revCurrKP1 = SequenceUtils.reverseComplement(currKP1);
+							int currKP1Int = RegionFileUtilities.seq2int(currKP1);
+							int revCurrKP1Int = RegionFileUtilities.seq2int(revCurrKP1);
+							int kmerP1 = currKP1Int < revCurrKP1Int? currKP1Int : revCurrKP1Int;
+							for(int s=j+1;s<(motif.length()-k+1);s++){
+								String currKP2 = motif.substring(s, s+k);
+								String revCurrKP2 = SequenceUtils.reverseComplement(currKP2);
+								int currKP2Int = RegionFileUtilities.seq2int(currKP2);
+								int revCurrKP2Int = RegionFileUtilities.seq2int(revCurrKP2);
+								int kmerP2 = currKP2Int < revCurrKP2Int ? currKP2Int: revCurrKP2Int;
+								int x = kmerP1 < kmerP2 ? kmerP1: kmerP2;
+								int y = kmerP1 < kmerP2 ? kmerP2: kmerP1;
+								score = score+kmerpairwights[x][y];
+							}
+							
+						}
+					}
+					
 					Region hill = new Region(gcon.getGenome(),r.getChrom(),r.getStart()+i,r.getStart()+i+l);
 					
 					Iterator<Pair<Region,Double>> it = mountains.iterator();
@@ -150,13 +176,27 @@ public class KmerModelScanner {
 		List<Region> rs = RegionFileUtilities.loadRegionsFromPeakFile(gcon.getGenome(), peaksFile, win);
 		String weightsFile = Args.parseString(args, "weights", "");
 		double[] ws = new double[(int) Math.pow(4, k)];
+		double[][] pws=null;
+		boolean isPair=false;
+		if(ap.hasKey("PairModel")){
+			isPair = true;
+		}
 		BufferedReader reader = new BufferedReader(new FileReader(weightsFile));
         String line;
         while ((line = reader.readLine()) != null) {
             line = line.trim();
             String[] words = line.split("\t");
-            int kind = RegionFileUtilities.seq2int(words[0]);
-            ws[kind] = Double.parseDouble(words[1]);
+            if(!isPair || !words[0].contains("-")){
+            	int kind = RegionFileUtilities.seq2int(words[0]);
+            	ws[kind] = Double.parseDouble(words[1]);
+            }else if(isPair && words[0].contains("-")){
+            	String[] subwords = words[0].split("-");
+            	int kp1ind = RegionFileUtilities.seq2int(subwords[0]);
+            	int kp2ind = RegionFileUtilities.seq2int(subwords[1]);
+            	pws = new double[(int) Math.pow(4, k)][(int) Math.pow(4, k)];
+            	pws[kp1ind][kp2ind] =  Double.parseDouble(words[1]);
+            	pws[kp2ind][kp1ind] = Double.parseDouble(words[1]);
+            }
         }
         boolean cache=false;
         String genPath = "";
@@ -168,6 +208,9 @@ public class KmerModelScanner {
         KmerModelScanner scanner = new KmerModelScanner(gcon);
         scanner.setK(k);
         scanner.setKmerWeights(ws);
+        if(isPair)
+        	scanner.setKmerPairWeights(pws);
+        scanner.setModelType(isPair);
         scanner.setmaxM(M);
         scanner.setminM(m);
         scanner.setPeaks(ps);
