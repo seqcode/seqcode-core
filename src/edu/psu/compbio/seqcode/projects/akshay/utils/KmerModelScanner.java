316422c600905dee5faeb10bddfd30d387fab0f7
auk262 <auk262@psu.edu>
2015-08-24 16:17:32 -0400

    Minor edits

--- src/edu/psu/compbio/seqcode/projects/akshay/utils/KmerModelScanner.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/utils/KmerModelScanner.java
@@ -209,7 +209,7 @@ public class KmerModelScanner {
 			double[] scorelanscape = new double[r.getWidth()];
 			for(int i=0; i<scorelanscape.length; i++){
 				int spanStart = i-k+1 >=0 ?  i-k+1 : 0;
-				int spanEnd = i+k-1 <=scorelanscape.length ?  i+k-1 : scorelanscape.length-k;
+				int spanEnd = i+k-1 <scorelanscape.length ?  i+k-1 : scorelanscape.length-k;
 				double spanScore=0;
 				int spanLenght = spanEnd-spanStart+1;
 				for(int j=spanStart; j<=spanEnd; j++){
