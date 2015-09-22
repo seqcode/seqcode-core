7e8c18f077bae3d8c4a2c21c73fdef69ff20482f
Naomi <nuy11@psu.edu>
2015-09-22 17:16:40 -0400

    debugging DifferentialMSR

--- src/edu/psu/compbio/seqcode/projects/naomi/DifferentialMSR.java
+++ src/edu/psu/compbio/seqcode/projects/naomi/DifferentialMSR.java
@@ -124,8 +124,13 @@ public class DifferentialMSR {
 			
 			//lets check up to here
 			System.out.println("current Chrom is: "+currChrom.getChrom());
+			
+			if (currChrom.getChrom()=="1")
+				for (int i = 0; i< 200;i++)
+					System.out.println(GaussianBlur[92943501+i]+"\t");
+			
 			for (int i = 0; i< 200;i++)
-				System.out.println(GaussianBlur[92943501+i]+"\t");
+				System.out.println(GaussianBlur[i]+"\t");
 			
 			/*********************
 			 * Starting nodes
