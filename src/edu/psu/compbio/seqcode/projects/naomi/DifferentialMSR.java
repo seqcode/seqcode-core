63779664e84cf7c433c3b39d9225f22f99f496dd
Naomi <nuy11@psu.edu>
2015-09-23 14:49:47 -0400

    fixing DifferentialMSR

--- src/edu/psu/compbio/seqcode/projects/naomi/DifferentialMSR.java
+++ src/edu/psu/compbio/seqcode/projects/naomi/DifferentialMSR.java
@@ -124,11 +124,13 @@ public class DifferentialMSR {
 			for (Sample sample : manager.getSamples()){		
 				List<StrandedBaseCount> currentCounts = sampleCountsMap.get(sample);
 				for (StrandedBaseCount hits: currentCounts){
-					for (int i = 0; i<threePrimReadExt+1; i++)
-						if (hits.getStrand()=='+' && hits.getCoordinate()+i<currchromSize)
+					for (int i = 0; i<threePrimReadExt+1; i++){
+						if (hits.getStrand()=='+' && hits.getCoordinate()+i<currchromSize){
 								GaussianBlur[(int) Math.ceil((hits.getCoordinate()+i)/binWidth)][1]+=hits.getCount();
-						else if (hits.getStrand()=='+' && hits.getCoordinate()-i >=0)
+						}else if (hits.getStrand()=='+' && hits.getCoordinate()-i >=0){
 								GaussianBlur[(int) Math.ceil((hits.getCoordinate()-i)/binWidth)][1]+=hits.getCount();
+						}
+					}
 				}
 				currentCounts = null;
 			}
