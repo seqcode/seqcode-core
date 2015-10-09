da2dd50b686ea058175d80b08ae92c623686e688
ikaka89 <akshay.kakumanu@gmail.com>
2015-10-08 23:45:50 -0400

    Added more function to PeaksVsPeaks code

--- src/edu/psu/compbio/seqcode/projects/akshay/utils/PeaksVsPeaks.java
+++ src/edu/psu/compbio/seqcode/projects/akshay/utils/PeaksVsPeaks.java
@@ -1,5 +1,7 @@
 package edu.psu.compbio.seqcode.projects.akshay.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import edu.psu.compbio.seqcode.genome.Genome;
@@ -18,6 +20,9 @@ public class PeaksVsPeaks {
 	protected List<Point> peaksB;
 	protected List<Region> regionsB;
 	
+	protected List<List<Point>> peaksBSet;
+	protected List<List<Region>> regionsBSet;
+	
 	protected int overlapD; // min distance between the closest peaks to call them overlapping 
 	protected int win; // for expanding a peak into a peak region	
 	
@@ -34,6 +39,8 @@ public class PeaksVsPeaks {
 	public void setRegsB(List<Region>  b){regionsB = b;}
 	public void setOverlapD(int o){overlapD =o;}
 	public void setWin(int w){win = w;}
+	public void setPeaksBSet(List<List<Point>> pset){peaksBSet = pset;}
+	public void setRegionsBSet(List<List<Region>> rset){regionsBSet = rset;}
 	
 	
 	public void printClosestPeaks(){
@@ -59,43 +66,92 @@ public class PeaksVsPeaks {
 		
 	}
 	
+	public void printPeakSets(){
+		for(Point pa: peaksA){
+			StringBuilder sb= new StringBuilder();
+			sb.append(pa.getLocationString());sb.append("\t");
+			Point[] nearestPeaks = new Point[peaksBSet.size()];
+			int[] nearestDis = new int[peaksBSet.size()];
+			Arrays.fill(nearestDis, Integer.MAX_VALUE);
+			int setId=0;
+			for(List<Point> bset: peaksBSet){
+				for(Point pb : bset){
+					if(pa.distance(pb) < nearestDis[setId]){
+						nearestDis[setId] = pa.distance(pb);
+						nearestPeaks[setId] =pb;
+					}
+				}
+				setId++;
+			}
+			for(int i=0; i<nearestDis.length;i++){
+				if(nearestDis[i]<=overlapD){
+					sb.append(nearestPeaks[i].getLocationString());sb.append("\t");
+				}else{
+					sb.append(nearestPeaks[i].getLocationString());sb.append("\t");
+				}
+			}
+			sb.deleteCharAt(sb.length()-1);
+			System.out.println(sb.toString());
+		}
+	}
+	
+	
+	
 	public static void main(String[] args){
 		GenomeConfig gconfig = new GenomeConfig(args);
 		ArgParser ap = new ArgParser(args);
 		
-		String peaksAfile = Args.parseString(args, "peaksA", null);
-		if(peaksAfile == null){
-			System.err.println("Provide ChIP-Seq peaks file!!");
-			return;
-		}
-		
-		String peaksBfile = Args.parseString(args, "peaksB", null);
-		if(peaksBfile == null){
-			System.err.println("Provide ChIP-Seq peaks file!!");
-			return;
-		}
-		
 		int win = Args.parseInteger(args, "win", 150);
-		
-		List<Point> peaksA = RegionFileUtilities.loadPeaksFromPeakFile(gconfig.getGenome(), peaksAfile, win);
-		List<Point> peaksB = RegionFileUtilities.loadPeaksFromPeakFile(gconfig.getGenome(), peaksBfile, win);
-		
-		List<Region> regionsA = RegionFileUtilities.loadRegionsFromPeakFile(gconfig.getGenome(), peaksAfile, win);
-		List<Region> regionsB = RegionFileUtilities.loadRegionsFromPeakFile(gconfig.getGenome(), peaksBfile, win);
-		
 		
 		PeaksVsPeaks analyzer = new PeaksVsPeaks(gconfig.getGenome());
 		
+		String peaksAfile = Args.parseString(args, "peaksA", null);
+		if(peaksAfile == null){
+			System.err.println("Provide ChIP-Seq peaks file A!!");
+			return;
+		}
+		List<Point> peaksA = RegionFileUtilities.loadPeaksFromPeakFile(gconfig.getGenome(), peaksAfile, win);
+		List<Region> regionsA = RegionFileUtilities.loadRegionsFromPeakFile(gconfig.getGenome(), peaksAfile, win);
 		analyzer.setPeaksA(peaksA);
-		analyzer.setPeaksB(peaksB);
 		analyzer.setRegsA(regionsA);
-		analyzer.setRegsB(regionsB);
+		
+		if(!ap.hasKey("peaksB") && !ap.hasKey("peaksSetB")){
+			System.err.println("Provide ChIP-Seq peaks file to compare with the A list!!");
+			return;
+		}
+		
+		String peaksBfile = null;
+		if(ap.hasKey("peaksB")){
+			peaksBfile = Args.parseString(args, "peaksB", null);
+			List<Point> peaksB = RegionFileUtilities.loadPeaksFromPeakFile(gconfig.getGenome(), peaksBfile, win);
+			List<Region> regionsB = RegionFileUtilities.loadRegionsFromPeakFile(gconfig.getGenome(), peaksBfile, win);
+			analyzer.setPeaksB(peaksB);
+			analyzer.setRegsB(regionsB);
+		}
+		
+		String[] peaksBfileList = null;
+		if(ap.hasKey("peaksBset")){
+			peaksBfileList =  ap.getKeyValue("peaksBset").split(";");
+			List<List<Point>> bPeaksSet = new ArrayList<List<Point>>();
+			List<List<Region>> bRegionSet = new ArrayList<List<Region>>();
+			for(int s=0; s<peaksBfileList.length; s++){
+				List<Point> currPoints = RegionFileUtilities.loadPeaksFromPeakFile(gconfig.getGenome(), peaksBfileList[s], win);
+				bPeaksSet.add(currPoints);
+				List<Region> currRegions = RegionFileUtilities.loadRegionsFromPeakFile(gconfig.getGenome(), peaksBfileList[s], win);
+				bRegionSet.add(currRegions);
+			}
+			analyzer.setPeaksBSet(bPeaksSet);
+			analyzer.setRegionsBSet(bRegionSet);
+		}
+		
 		
 		int overlapD = Args.parseInteger(args, "overlapD", 100);
 		analyzer.setOverlapD(overlapD);
 		
-		
-		analyzer.printClosestPeaks();
+		if(ap.hasKey("peaksB"))
+			analyzer.printClosestPeaks();
+		if(ap.hasKey("peaksBset"))
+			analyzer.printPeakSets();
 	}
 	
 	
