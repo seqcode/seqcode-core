6dcfc55ccb4cf2ceed0dbebd4e21bec4c9714616
auk262 <auk262@psu.edu>
2016-02-09 16:16:12 -0500

    Adding code to directly generate arff file for SeqUnwinder

---
+++ src/edu/psu/compbio/seqcode/projects/akshay/SeqUnwinder/loadData/MakeArff.java
@@ -0,0 +1,30 @@
+package edu.psu.compbio.seqcode.projects.akshay.SeqUnwinder.loadData;
+
+import edu.psu.compbio.seqcode.genome.GenomeConfig;
+import edu.psu.compbio.seqcode.gse.utils.ArgParser;
+
+public class MakeArff {
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args){
+		
+		GenomeConfig gc = new GenomeConfig(args);
+		ArgParser ap = new ArgParser(args);
+		String eventsFile = ap.getKeyValue("peaks");
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
