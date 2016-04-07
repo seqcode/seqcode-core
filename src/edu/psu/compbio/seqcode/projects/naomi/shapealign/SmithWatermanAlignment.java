0ac700dbe9fe50183a72685d442aa7ce01f67b77
Naomi <nuy11@psu.edu>
2016-04-07 10:53:08 -0400

    testing

--- src/edu/psu/compbio/seqcode/projects/naomi/shapealign/SmithWatermanAlignment.java
+++ src/edu/psu/compbio/seqcode/projects/naomi/shapealign/SmithWatermanAlignment.java
@@ -122,7 +122,7 @@ public class SmithWatermanAlignment {
 //			for (int i = 0; i <regions.size();i++){		
 			
 //			for (int i = 0; i <1 ; i++){	
-			for (int i = 1; i <2 ; i++){
+			for (int i = 2; i <3 ; i++){
 				for (int j = i+1; j <regions.size();j++)
 					smithWatermanAlgorithm(sample, regions.get(i), regions.get(j));				
 			}
