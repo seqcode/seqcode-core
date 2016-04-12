519dad66146ef20491d2a49716aab1cd05e67aa2
Naomi <nuy11@psu.edu>
2016-04-12 11:12:08 -0400

    Test with eucledian based similarity metrics.

--- src/edu/psu/compbio/seqcode/projects/naomi/shapealign/SmithWatermanAlignmentMatrix.java
+++ src/edu/psu/compbio/seqcode/projects/naomi/shapealign/SmithWatermanAlignmentMatrix.java
@@ -26,7 +26,7 @@ public class SmithWatermanAlignmentMatrix {
 //	final static double GAP_OPEN = 1;
 //	final static double GAP_EXT = 0.5;
 	
-	final static double GAP_OPEN = 5;
+	final static double GAP_OPEN = 4;
 	final static double GAP_EXT = 2;
 	final static double WEIGHT = 40;
 	
@@ -60,21 +60,30 @@ public class SmithWatermanAlignmentMatrix {
 	public void setEndXCoord(int endXCoord){alignEndXCoord = endXCoord;}
 	public void setEndYCoord(int endYCoord){alignEndYCoord = endYCoord;}
 	
-//	public double computeScore(double aVal, double bVal){
-//		double score = (aVal + bVal)/2 - Math.abs(aVal - bVal);
-//		return score;		
-//	}
+	
+	public double computeSimilarityScore(double x1, double x2, double y1, double y2){
+		
+		double score = 1/Math.sqrt(Math.abs(Math.pow(x1-x2, 2)+Math.pow(y1-y2,2)))*((x1+x2+y1+y2)/2)-Math.abs(x1-x2)-Math.abs(y1-y2);		
+		return score;
+	
+	}
+	
 	
 	public double computeScore(double aVal, double bVal){
+		double score = (aVal + bVal)/2 - Math.abs(aVal - bVal);
+		return score;		
+	}
 	
-	double score = MINIMUM_VALUE;
-	if (aVal != bVal ){	
-		score = 1/Math.sqrt(Math.abs(Math.pow(aVal,2)-Math.pow(bVal, 2)))*Math.pow((aVal+bVal)/2, 2)-Math.abs(aVal-bVal);
-	}else{
-		score = WEIGHT*Math.pow((aVal+bVal)/2, 2)-Math.abs(aVal-bVal);
-	}		
-	return score;
-	}	
+//	public double computeScore(double aVal, double bVal){
+	
+//	double score = MINIMUM_VALUE;
+//	if (aVal != bVal ){	
+//		score = 1/Math.sqrt(Math.abs(Math.pow(aVal,2)-Math.pow(bVal, 2)))*Math.pow((aVal+bVal)/2, 2)-Math.abs(aVal-bVal);
+//	}else{
+//		score = WEIGHT*Math.pow((aVal+bVal)/2, 2)-Math.abs(aVal-bVal);
+//	}		
+//	return score;
+//	}	
 	
 	// this needs to go with big GAP_OPEN and GAP_EXT
 //	public double computeScore(double aVal, double bVal){
@@ -100,8 +109,11 @@ public class SmithWatermanAlignmentMatrix {
 		
 		for (int i = 1 ; i <= window; i++){
 			for (int j = 1 ; j <= window ; j++){
-				double mScore = computeScore(regACounts[i-1][0], regBCounts[j-1][0])
-						+ computeScore(regACounts[i-1][1], regBCounts[j-1][1]);
+				
+				double mScore = computeSimilarityScore(regACounts[i-1][0],regBCounts[j-1][0],regACounts[i-1][1],regBCounts[j-1][1]);
+				
+//				double mScore = computeScore(regACounts[i-1][0], regBCounts[j-1][0])
+//						+ computeScore(regACounts[i-1][1], regBCounts[j-1][1]);
 			
 				double temp_M[] = new double[3];
 				temp_M[0] = M[i-1][j-1] + mScore;
