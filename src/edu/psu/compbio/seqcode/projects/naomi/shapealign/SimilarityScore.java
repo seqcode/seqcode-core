2b6d1d9a715ccd9bfb659523d04ad390bf7b19ae
Naomi <nuy11@psu.edu>
2016-05-09 19:07:55 -0400

    Added various similarity score calculations.

--- src/edu/psu/compbio/seqcode/projects/naomi/shapealign/SimilarityScore.java
+++ src/edu/psu/compbio/seqcode/projects/naomi/shapealign/SimilarityScore.java
@@ -15,14 +15,20 @@ public class SimilarityScore {
 	double similarity_score;
 	
 	protected boolean euclidean_L2 = false;
+	protected boolean sorensen = false;
+	protected boolean soergel = false;
+	protected boolean lorentzian = false;
+	protected boolean pce = false;
+	protected boolean squared_chi = false;
+	protected boolean divergence = false;
+	protected boolean clark = false;
 	
 	public SimilarityScore(double x_1, double x_2, double y_1, double y_2){
 		
 		this.x1 = x_1;
 		this.x2 = x_2;
 		this.y1 = y_1;
-		this.y2 = y_2;		
-		
+		this.y2 = y_2;				
 	}
 	
 	// accessors
@@ -30,81 +36,87 @@ public class SimilarityScore {
 	
 	// setter
 	public void setEuclideanL2(){euclidean_L2 = true;}
+	public void setSorensen(){sorensen = true;}
+	public void setSoergel(){soergel = true;}
+	public void setLorentzian(){lorentzian = true;}
+	public void setPCE(){pce = true;}
+	public void setSquared_chi(){squared_chi = true;}
+	public void setDivergence(){divergence = true;}
+	public void setClark(){clark = true;}
 	
 	public double computeScore(){
 		
 		double score = 0;
 		if ( (x1 != x2) || (y1 != y2)){	
 			if (euclidean_L2 == true){score = euclideanL2();}
-			
+			else if (sorensen == true){score = sorensen();}
+			else if (soergel == true){score = soergel();}
+			else if (lorentzian == true){score = lorentzian();}
+			else if (pce == true){score = PCE();}
+			else if (squared_chi == true){score = squared_chi();}
+			else if (divergence == true){score = divergence();}
+			else if (clark == true){score = clark();}
+			else{ score = linear();}			
 		}
-
 		return score;
 
 	}
 	
-	
+	public double linear(){
+
+		double score = (x1 + x2)/2 + (y1 + y2)/2 - (Math.abs(x1 - x2) + Math.abs(y1 - y2));
+		return score;
+		
+	}
+		
 	public double euclideanL2(){
 		
-		double score = 1/(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2)) - Math.abs(x1-x2)-Math.abs(y1-y2);
-		
+		double score = 1/(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2)) - Math.abs(x1-x2)-Math.abs(y1-y2);		
 		return score;		
 	}
 	
 	public double sorensen(){
 		
-		double score = (x1+x2+y1+y2)/(Math.abs(x1-x2) + Math.abs(y1-y2)) - Math.abs(x1-x2)-Math.abs(y1-y2);
-		
+		double score = (x1+x2+y1+y2)/(Math.abs(x1-x2) + Math.abs(y1-y2)) - Math.abs(x1-x2)-Math.abs(y1-y2);		
 		return score;		
 	}
 	
 	public double soergel(){
 		
-		double score = (Math.max(x1, x2) + Math.max(y1, y2))/(Math.abs(x1-x2) + Math.abs(y1-y2)) - Math.abs(x1-x2)-Math.abs(y1-y2);
-		
+		double score = (Math.max(x1, x2) + Math.max(y1, y2))/(Math.abs(x1-x2) + Math.abs(y1-y2)) - Math.abs(x1-x2)-Math.abs(y1-y2);		
 		return score;		
 	}
 	
 	public double lorentzian(){
 		
-		double score = 1/(Math.log(1+Math.abs(x1-y2))+Math.log(1+Math.abs(y1-y2))) - Math.abs(x1-x2)-Math.abs(y1-y2);
-		
+		double score = 1/(Math.log(1+Math.abs(x1-y2))+Math.log(1+Math.abs(y1-y2))) - Math.abs(x1-x2)-Math.abs(y1-y2);		
 		return score;		
 	}
 	
 	public double PCE(){
 		
-		double pce =  (x1*x2 + y1*y2)/(Math.pow(x1, 2) + Math.pow(y1, 2) + Math.pow(x2, 2) + Math.pow(y2, 2) - ( x1*x2 + y1*y2 ));
-				
-		double score = pce - Math.abs(x1-x2)-Math.abs(y1-y2);
-		
+		double pce =  (x1*x2 + y1*y2)/(Math.pow(x1, 2) + Math.pow(y1, 2) + Math.pow(x2, 2) + Math.pow(y2, 2) - ( x1*x2 + y1*y2 ));				
+		double score = pce - Math.abs(x1-x2)-Math.abs(y1-y2);		
 		return score;		
 	}
 	
 	public double squared_chi(){
 
-		double score =  1/(Math.pow(x1-x2, 2)/(x1+x2) + Math.pow(y1-y2, 2)/(y1+y2)) - Math.abs(x1-x2)-Math.abs(y1-y2);
-		
+		double score =  1/(Math.pow(x1-x2, 2)/(x1+x2) + Math.pow(y1-y2, 2)/(y1+y2)) - Math.abs(x1-x2)-Math.abs(y1-y2);		
 		return score;
 	}
 	
 	public double divergence(){
 
-		double denom =  2*(Math.pow(x1-x2, 2)/Math.pow(x1+x2,2) + Math.pow(y1-y2, 2)/Math.pow(y1+y2,2));
-				
-		double score = 1/denom - Math.abs(x1-x2)-Math.abs(y1-y2);
-		
+		double denom =  2*(Math.pow(x1-x2, 2)/Math.pow(x1+x2,2) + Math.pow(y1-y2, 2)/Math.pow(y1+y2,2));				
+		double score = 1/denom - Math.abs(x1-x2)-Math.abs(y1-y2);		
 		return score;
 	}
 	
 	public double clark(){
 
-		double denom =  Math.sqrt(Math.pow(Math.abs(x1-x2)/(x1+x2), 2) + Math.pow(Math.abs(y1-y2)/(y1+y2), 2));
-				
-		double score = 1/denom - Math.abs(x1-x2)-Math.abs(y1-y2);
-		
+		double denom =  Math.sqrt(Math.pow(Math.abs(x1-x2)/(x1+x2), 2) + Math.pow(Math.abs(y1-y2)/(y1+y2), 2));				
+		double score = 1/denom - Math.abs(x1-x2)-Math.abs(y1-y2);		
 		return score;
-	}
-	
-
+	}	
 }
--- src/edu/psu/compbio/seqcode/projects/naomi/shapealign/SmithWatermanAlignmentMatrix.java
+++ src/edu/psu/compbio/seqcode/projects/naomi/shapealign/SmithWatermanAlignmentMatrix.java
@@ -60,51 +60,7 @@ public class SmithWatermanAlignmentMatrix {
 	public void setEndXCoord(int endXCoord){alignEndXCoord = endXCoord;}
 	public void setEndYCoord(int endYCoord){alignEndYCoord = endYCoord;}
 	
-	
-	public double computeSimilarityScore(double x1, double x2, double y1, double y2){
-		
-		double score = 0;
 
-		if ( ( x1 == x2 ) && (y1 == y2)){
-			 score = 0;
-		}else{
-			 score = 1/Math.sqrt(Math.abs(Math.pow(x1-x2, 2)+Math.pow(y1-y2,2)))*((x1+x2+y1+y2)/4)-Math.abs(x1-x2)-Math.abs(y1-y2);
-		}	
-		return score;
-	
-	}
-	
-	
-	public double computeScore(double x1, double x2, double y1, double y2){
-		
-		double score = (x1 + x2)/2 + (y1 + y2)/2 - (Math.abs(x1 - x2) + Math.abs(y1 - y2));
-		return score;		
-	}
-	
-//	public double computeScore(double aVal, double bVal){
-	
-//	double score = MINIMUM_VALUE;
-//	if (aVal != bVal ){	
-//		score = 1/Math.sqrt(Math.abs(Math.pow(aVal,2)-Math.pow(bVal, 2)))*Math.pow((aVal+bVal)/2, 2)-Math.abs(aVal-bVal);
-//	}else{
-//		score = WEIGHT*Math.pow((aVal+bVal)/2, 2)-Math.abs(aVal-bVal);
-//	}		
-//	return score;
-//	}	
-	
-	// this needs to go with big GAP_OPEN and GAP_EXT
-//	public double computeScore(double aVal, double bVal){
-		
-//		double score = MINIMUM_VALUE;
-//		if (aVal != bVal ){	
-//			score = 1/Math.sqrt(Math.pow(aVal-bVal, 2))*Math.pow((aVal+bVal)/2, 2)-Math.abs(aVal-bVal);
-//		}else{
-//			score = 100*Math.pow((aVal+bVal)/2, 2)-Math.abs(aVal-bVal);
-//		}		
-//		return score;
-//	}	
-	
-	
 	public void buildMatrix(){
 		
 		//initialization of M matrix
@@ -117,7 +73,8 @@ public class SmithWatermanAlignmentMatrix {
 		for (int i = 1 ; i <= window; i++){
 			for (int j = 1 ; j <= window ; j++){
 				
-				double mScore = computeScore(regACounts[i-1][0],regBCounts[j-1][0],regACounts[i-1][1],regBCounts[j-1][1]);
+				SimilarityScore similarity_s = new SimilarityScore(regACounts[i-1][0],regBCounts[j-1][0],regACounts[i-1][1],regBCounts[j-1][1]);
+				double mScore = similarity_s.computeScore();
 			
 				double temp_M[] = new double[3];
 				temp_M[0] = M[i-1][j-1] + mScore;
@@ -174,7 +131,8 @@ public class SmithWatermanAlignmentMatrix {
 		
 		while ( i != 0 && j != 0){
 
-			double mScore = computeScore(regACounts[i-1][0],regBCounts[j-1][0],regACounts[i-1][1],regBCounts[j-1][1]);
+			SimilarityScore similarity_s = new SimilarityScore(regACounts[i-1][0],regBCounts[j-1][0],regACounts[i-1][1],regBCounts[j-1][1]);
+			double mScore = similarity_s.computeScore();
 			
 			// diagonal case
 			if ( M[i-1][j-1] + mScore == currentScore ){
@@ -195,129 +153,6 @@ public class SmithWatermanAlignmentMatrix {
 				System.out.println("M value is"+M[i-1][j]);
 				traceBack.push(UP);
 				currentScore = M[i-1][j];
-				i--;
-			}
-			
-			current_x = i;
-			current_y = j;
-		}
-		
-		setMaxScore(maxScore);
-		setTraceBack(traceBack);
-		setStartXCoord(current_x);
-		setStartYCoord(current_y);
-		setEndXCoord(x_coord);
-		setEndYCoord(y_coord);
-	}
-
-
-	// builds M and I matrix; this is currently not working
-	public void buildGappedMatrix(){
-		
-		//initialization of M[][] & I[][] & H[][] matrix
-		double [][] M = new double [window+1][window+1];
-		double [][] I = new double [window+1][window+1];
-		for (int i = 0 ; i <= window; i++){
-			for (int j = 0 ; j <= window; j++){
-				M[i][j] = 0;
-				I[i][j] = 0;
-			}
-		}	
-
-
-		//fill in M[i][j] & I[i][j] matrix
-		for (int i = 1 ; i <= window; i++){
-			for (int j = 1 ; j <= window ; j++){
-				double mScore = computeScore(regACounts[i-1][0], regBCounts[i-1][0],regACounts[j-1][1], regBCounts[j-1][1]);
-			
-				M[i][j] = Math.max(M[i-1][j-1] + mScore, I[i-1][j-1] + mScore);
-			
-				double temp_I[] = new double[4];
-				temp_I[0] = M[i][j-1] - GAP_OPEN;
-				temp_I[1] = I[i][j-1] - GAP_EXT;
-				temp_I[2] = M[i-1][j] - GAP_OPEN;
-				temp_I[3] = I[i-1][j] - GAP_EXT;
-			
-				double max_I = MINIMUM_VALUE;
-				for (int k = 0 ; k < 4 ; k++){
-					if (temp_I[k] > max_I){ max_I = temp_I[k];}
-				}
-				
-				I[i][j] = max_I;
-			}
-		}
-
-		
-		System.out.println("printing M matrix");
-		for (int i = 0; i <=window ; i++){
-			for (int j = 0; j <=window; j++){
-				System.out.print(M[i][j]+" ");
-			}
-			System.out.println();
-		}
-		
-		// find the highest value
-		double maxScore = MINIMUM_VALUE;
-		int x_coord = 0;
-		int y_coord = 0;
-	
-		for (int i = (int) Math.floor(window/2); i <= window; i++){
-			if (M[i][window] > maxScore){
-				maxScore = M[i][window];
-				x_coord = i;
-				y_coord = window;	
-			}
-			if (I[i][window] > maxScore){
-				maxScore = I[i][window];
-				x_coord = i;
-				y_coord = window;
-			}
-		}
-		for (int j = (int) Math.floor(window/2); j <= window; j++){
-			if (M[window][j] > maxScore){
-				maxScore = M[window][j];
-				x_coord = j;
-				y_coord = window;	
-			}
-			if (I[window][j] > maxScore){
-				maxScore = I[window][j];
-				x_coord = j;
-				y_coord = window;
-			}
-		}
-		
-		System.out.println("max score is "+maxScore+" x coord is "+x_coord+" y coord is "+y_coord);
-		
-		// back track to reconstruct the path
-		double currentScore = maxScore;
-		int current_x = x_coord;
-		int current_y = y_coord;
-		Stack<Integer> traceBack = new Stack<Integer>();
-		
-		int i = x_coord;
-		int j = y_coord;
-		
-		while ( i != 0 && j != 0){
-
-			double mScore = computeScore(regACounts[i-1][0], regBCounts[j-1][0],regACounts[i-1][1], regBCounts[j-1][1]);
-			
-			// diagonal case
-			if ( (M[i-1][j-1] + mScore == currentScore) || (I[i-1][j-1] + mScore == currentScore)){
-				traceBack.push(DIAG);		
-				currentScore = Math.max(M[i-1][j-1], I[i-1][j-1]);
-				i--;
-				j--;
-			
-			// left case
-			}else if( (M[i][j-1]-GAP_OPEN == currentScore) || (I[i][j-1]-GAP_EXT == currentScore)){
-				traceBack.push(LEFT);
-				currentScore = Math.max(M[i][j-1], I[i][j-1]);
-				j--;
-			
-			// right case
-			}else{
-				traceBack.push(UP);
-				currentScore = Math.max(M[i-1][j], I[i-1][j]);
 				i--;
 			}
 			
