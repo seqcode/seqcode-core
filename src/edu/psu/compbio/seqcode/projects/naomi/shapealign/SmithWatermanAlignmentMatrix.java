4a595a8fe0b33906779d1dcead81ab1a1d16f9fc
Naomi <nuy11@psu.edu>
2016-05-10 14:43:51 -0400

    minor bug fix

--- src/edu/psu/compbio/seqcode/projects/naomi/shapealign/SmithWatermanAlignmentMatrix.java
+++ src/edu/psu/compbio/seqcode/projects/naomi/shapealign/SmithWatermanAlignmentMatrix.java
@@ -24,7 +24,7 @@ public class SmithWatermanAlignmentMatrix {
 	protected int alignEndXCoord;
 	protected int alignEndYCoord;
 	
-	static final double GAP_OPEN = 100;	
+	static final double GAP_OPEN = 1000;	
 	static final int DIAG = 1;
 	static final int LEFT = 2;
 	static final int UP = 4;	
