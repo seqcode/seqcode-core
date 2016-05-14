143e7aa851a22188bf794bc7977a2cf592f52f98
Naomi <nuy11@psu.edu>
2016-05-14 15:07:13 -0400

    Minor bug fix.

--- src/edu/psu/compbio/seqcode/projects/naomi/xoqualitycontrol/ReadDistributionProfiler.java
+++ src/edu/psu/compbio/seqcode/projects/naomi/xoqualitycontrol/ReadDistributionProfiler.java
@@ -109,9 +109,9 @@ public class ReadDistributionProfiler {
 				}else{ // if regions (features) are reverse strand, I need to flip the strands and locations
 					for (StrandedBaseCount hits: sampleCountsMap.get(sample).get(reg)){	
 						if (hits.getStrand()=='-'){
-							sampleCounts[reg.getMidpoint().getLocation()-hits.getCoordinate()][1] = hits.getCount();
+							sampleCounts[reg.getMidpoint().getLocation()-hits.getCoordinate()+window/2][1] = hits.getCount();
 						}else{
-							sampleCounts[reg.getMidpoint().getLocation()-hits.getCoordinate()][0] = hits.getCount();	
+							sampleCounts[reg.getMidpoint().getLocation()-hits.getCoordinate()+window/2][0] = hits.getCount();	
 						}			
 					}		
 				}
