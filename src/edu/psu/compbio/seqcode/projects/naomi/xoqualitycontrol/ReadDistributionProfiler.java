a66a36640985d79e71524f800485ca74050c8b84
Naomi <nuy11@psu.edu>
2016-05-16 12:09:09 -0400

    Removed print statements that were used for debugging.

--- src/edu/psu/compbio/seqcode/projects/naomi/xoqualitycontrol/ReadDistributionProfiler.java
+++ src/edu/psu/compbio/seqcode/projects/naomi/xoqualitycontrol/ReadDistributionProfiler.java
@@ -135,22 +135,12 @@ public class ReadDistributionProfiler {
 			
 			for (Region reg : sampleCountsArray.get(sample).keySet()){				
 				double[][] regionCounts = sampleCountsArray.get(sample).get(reg);		
-				
-				System.out.println("size of regionCounts is "+regionCounts.length);
-				for (int i = 0 ; i <50; i++){
-					System.out.print(regionCounts[i][0]+"\t");
-				}
-				
+
 				// get shifted composite for forward and reverse strands
 				for (int j = 0 ; j <=window ; j++){
 					nonstrandedComposite[j] += regionCounts[j-fivePrimeShift+edge/2][0]; 
 					nonstrandedComposite[j] += regionCounts[j+fivePrimeShift+edge/2][1];
 				}
-				System.out.println("nonstrandedComposite");
-				for (int i = 0 ; i <50; i++){
-					System.out.print(nonstrandedComposite[i]+"\t");
-				}
-				System.out.println();
 			}
 			sampleComposite.put(sample, nonstrandedComposite);		
 		}
