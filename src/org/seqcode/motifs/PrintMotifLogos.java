d6734148b72e11da5e3329ec9ac2d6c30e3bc310
Naomi <nuy11@psu.edu>
2017-08-15 13:07:08 -0400

    Added a class to print motif logos (a copy of old class askshay wrote)

---
+++ src/org/seqcode/motifs/PrintMotifLogos.java
@@ -0,0 +1,65 @@
+package org.seqcode.motifs;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.seqcode.data.io.BackgroundModelIO;
+import org.seqcode.data.motifdb.CountsBackgroundModel;
+import org.seqcode.data.motifdb.MarkovBackgroundModel;
+import org.seqcode.data.motifdb.WeightMatrix;
+import org.seqcode.genome.GenomeConfig;
+import org.seqcode.gseutils.ArgParser;
+import org.seqcode.motifs.FreqMatrixImport;
+
+
+public class PrintMotifLogos {
+	
+	protected List<WeightMatrix> motifs=new ArrayList<WeightMatrix>();
+	
+	// Load freq matrices
+	public void loadMotifsFromFile(String filename, MarkovBackgroundModel b) {
+		FreqMatrixImport motifImport = new FreqMatrixImport();
+		motifImport.setBackground(b);
+		motifs.addAll(motifImport.readTransfacMatrices(filename));
+	}
+	
+	public void execute(boolean drawaxis){
+		// Finally, draw the motif logos
+		for(WeightMatrix fm : motifs){
+			File motifFileName = new File(fm.getName()+".png");
+			org.seqcode.motifs.DrawMotifs.printMotifLogo(fm, motifFileName, 150, fm.getName(), drawaxis);
+			motifFileName = new File(fm.getName()+"_rc.png");
+			org.seqcode.motifs.DrawMotifs.printMotifLogo(WeightMatrix.reverseComplement(fm), motifFileName, 150, fm.getName(), drawaxis);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException, ParseException{
+		
+		ArgParser ap = new ArgParser(args);
+		GenomeConfig gcon = new GenomeConfig(args);
+		String motfile = ap.getKeyValue("motfile");
+		String backFile =ap.hasKey("back") ? ap.getKeyValue("back"):null;
+		boolean showaxis = ap.hasKey("showaxix");
+
+		MarkovBackgroundModel back;
+
+		if(backFile == null){
+			back = new MarkovBackgroundModel(CountsBackgroundModel.modelFromWholeGenome(gcon.getGenome()));
+		}else{
+			back = BackgroundModelIO.parseMarkovBackgroundModel(backFile, gcon.getGenome());
+		}
+		
+		PrintMotifLogos plotter = new PrintMotifLogos();
+		plotter.loadMotifsFromFile(motfile, back);
+		
+		plotter.execute(showaxis);
+
+	}
+	
+	
+
+}
+
