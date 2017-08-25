8aae611e0318e342345d3bad275ea98b1bdac53a
Naomi <nuy11@psu.edu>
2017-08-25 18:01:10 -0400

    Refactored ChIPQC and RegionCountSorter used in YEP pipeline.

---
+++ src/org/seqcode/projects/galaxyexo/ChIPQC.java
@@ -0,0 +1,145 @@
+package org.seqcode.projects.galaxyexo;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.seqcode.deepseq.experiments.ControlledExperiment;
+import org.seqcode.deepseq.experiments.ExperimentCondition;
+import org.seqcode.deepseq.experiments.ExperimentManager;
+import org.seqcode.deepseq.experiments.ExptConfig;
+import org.seqcode.deepseq.experiments.Sample;
+import org.seqcode.genome.Genome;
+import org.seqcode.genome.GenomeConfig;
+import org.seqcode.genome.location.Region;
+import org.seqcode.gseutils.ArgParser;
+
+/**
+ * Utility to output a signal fraction calculated based on a normalization. It will also output a scaling factor, number of reads etc.
+ * It includes an option to output hit counts of signal and control experiments in a given bin size in genome wide.
+ * 
+ * Input:
+ * 		- Genome
+ * 		- Signal experiment
+ * 		- Control experiment
+ * Output:
+ * 		- A text file listing condition, signal hits, control hits, scaling factor, and a signal fraction.
+ * 
+ * @author naomi yamada
+ */
+public class ChIPQC {	
+	protected GenomeConfig gconfig;
+	protected ExptConfig econfig;
+	protected ExperimentManager manager;
+	
+	public ChIPQC(GenomeConfig gcon, ExptConfig econ, ExperimentManager man){	
+		gconfig = gcon;
+		econfig = econ;
+		manager = man;
+	}
+	 
+	public void printQCMetrics(){		
+		double ncis, signalHits, controlHits;
+		double IPstrength=0;
+				
+		for(ExperimentCondition exptCond: manager.getConditions()){
+			for(ControlledExperiment rep : exptCond.getReplicates()){
+				if (!rep.hasControl()){
+					System.err.println("Please provide a control experiment");
+					System.exit(1);
+				}				
+				ncis = rep.getControlScaling();
+				signalHits = rep.getSignal().getHitCount();
+				controlHits = rep.getControl().getHitCount();
+				IPstrength = 1-(ncis/(signalHits/controlHits));
+				if (IPstrength<0)
+					IPstrength=0;
+				System.out.println("Condition:"+rep.getCondName()+"\tSignal:"+signalHits+"\tControl:"+controlHits+"\tScalingFactor:"+ncis+"\tIPstrength: "+IPstrength);
+			}
+		}
+		manager.close();
+	}
+	
+	public void printPairedBinCounts(int scalingWindowSize) throws FileNotFoundException, UnsupportedEncodingException{
+		
+		Genome genome = econfig.getGenome();
+		Map<Sample, List<Float>> sampleWindowCounts = new HashMap<Sample, List<Float>>();
+		List<Sample> allSamples = new ArrayList<Sample>();
+		List<Sample> signalSamples = new ArrayList<Sample>();
+		List<Sample> controlSamples = new ArrayList<Sample>();
+			
+		for(ExperimentCondition exptCond: manager.getConditions()){			
+			for(ControlledExperiment rep : exptCond.getReplicates()){
+				signalSamples.add(rep.getSignal());
+				controlSamples.add(rep.getControl());
+			}
+			
+			allSamples.addAll(signalSamples);
+			allSamples.addAll(controlSamples);
+			int listSize=0;
+			for(Sample samp : allSamples){
+				List<Float> currSampCounts = new ArrayList<Float>();
+				for(String chrom:genome.getChromList()) {
+					int chrlen = genome.getChromLength(chrom);
+					for (int start = 1; start  < chrlen - scalingWindowSize; start += scalingWindowSize) {
+						Region r = new Region(genome, chrom, start, start + scalingWindowSize);
+						currSampCounts.add(samp.countHits(r));
+					}
+				}
+				sampleWindowCounts.put(samp, currSampCounts);
+				listSize = currSampCounts.size();
+			}
+		}
+		for(ExperimentCondition exptCond: manager.getConditions()){			
+			for(ControlledExperiment rep : exptCond.getReplicates()){				
+				PrintWriter writer = new PrintWriter(rep.getName()+rep.getCondName()+".counts.txt","UTF-8");
+				writer.println("#signalBinCounts : controlBinCounts"+"\t"+"#"+rep.getCondName());
+			
+				List<Float> signalSampCounts = new ArrayList<Float>();
+				List<Float> controlSampCounts = new ArrayList<Float>();
+				signalSampCounts = sampleWindowCounts.get(rep.getSignal());
+				controlSampCounts = sampleWindowCounts.get(rep.getControl());
+				for (int i = 0; i < signalSampCounts.size(); i ++){
+					if (signalSampCounts.get(i)+controlSampCounts.get(i)>0)
+						writer.println(signalSampCounts.get(i)+":"+controlSampCounts.get(i));				
+				}
+			}
+		}		
+		manager.close();
+	}
+		
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {	
+		/***
+		 * example: 
+		 * java org.seqcode.projects.galaxyexo.ChIPQC --species "Homo sapiens;hg19" --expt expt.bam --ctrl ctrl.bam --format BAM --scalewin 5000 --plotscaling --binCounts > NCIS.out
+		 ***/
+		ArgParser ap = new ArgParser(args);	
+        if((!ap.hasKey("species") && !ap.hasKey("geninfo"))) { 
+            System.err.println("Usage:\n" +
+                               "RegionCountSorter\n" +
+                               "\t--species <organism;genome> OR\n" +
+                               "\t--geninfo <genome info file> AND --seq <path to seqs>\n" +
+                               "\t--expt <experiments> \n" +
+                               "\nOPTIONS:\n" +
+                               "\t--scalewin <window size for scaling procedure (default=10000)>\n" +
+                               "\t--binCounts [flag to print bin counts] \n" +
+            				   "\t--plotscaling [flag to plot diagnostic information for the chosen scaling method]\n" +
+                               "");
+            System.exit(0);
+        }
+		
+		GenomeConfig gconf = new GenomeConfig(args);
+		ExptConfig  econf = new ExptConfig(gconf.getGenome(), args);		
+		econf.setPerBaseReadFiltering(false);		
+		ExperimentManager manager = new ExperimentManager(econf);
+		ChIPQC exoQC = new ChIPQC(gconf, econf, manager); 		
+		exoQC.printQCMetrics();			
+		if (ap.hasKey("binCounts"))
+			exoQC.printPairedBinCounts(econf.getScalingSlidingWindow());
+		manager.close();
+	}
+}
---
+++ src/org/seqcode/projects/galaxyexo/RegionCountSorter.java
@@ -0,0 +1,183 @@
+package org.seqcode.projects.galaxyexo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.seqcode.data.io.RegionFileUtilities;
+import org.seqcode.deepseq.experiments.ExperimentManager;
+import org.seqcode.deepseq.experiments.ExptConfig;
+import org.seqcode.deepseq.experiments.Sample;
+import org.seqcode.genome.Genome;
+import org.seqcode.genome.GenomeConfig;
+import org.seqcode.genome.location.Point;
+import org.seqcode.genome.location.Region;
+import org.seqcode.genome.location.StrandedRegion;
+import org.seqcode.gseutils.ArgParser;
+import org.seqcode.gseutils.Args;
+
+/**
+ * Utility to sort locations by tag counts.  
+ * If multiple samples are provided, experiments are normalized by total tags and locations are sorted based on average tag counts. 
+ * 
+ * input : Signal experiment
+ * 		 : Points
+ *       : Window size
+ * output: Regions sorted by tag counts
+ * 
+ * @author naomi yamada
+ */
+
+public class RegionCountSorter {
+	protected GenomeConfig gconf;
+	protected Genome genome;
+	protected ExperimentManager manager;
+	protected ExptConfig econf;
+	protected List<Point> points;
+	protected List<Region> regions; 
+	protected List<StrandedRegion> strandedReg;
+	protected int window;
+	
+	public RegionCountSorter(GenomeConfig gcon, ExperimentManager man, ExptConfig econ, int win){
+		gconf = gcon;
+		genome = gconf.getGenome();
+		manager= man;
+		econf = econ;
+		window = win;
+	}
+	
+	public void setPoint(List<Point> p ){ points = p;}
+	public void setStrandedRegion(List<StrandedRegion> sreg){ strandedReg=sreg; }
+	public void setRegion(List<Region> reg){ regions=reg;}
+	
+	public void execute(){
+		
+		// get the regions to count reads
+		List<Region> countRegions = new ArrayList<Region>();
+		if (points != null){
+			for (Point p : points)
+				countRegions.add(p.expand(window/2));
+		}else if (strandedReg != null){
+			if(window >0){
+				for (StrandedRegion sreg : strandedReg)					
+					countRegions.add(sreg.resize(window));
+			}else{
+				for (StrandedRegion sreg : strandedReg)
+					countRegions.add(sreg);
+			}
+		}else if(regions != null){
+			if (window>0){
+				for (Region reg : regions)
+					countRegions.add(reg.resize(window));
+			}else{
+				countRegions = regions;
+			}
+		}
+		
+		double sumReads = 0;
+		// summing total tag counts for all experiments
+		for (Sample sample : manager.getSamples())
+			sumReads += sample.getHitCount();
+		
+		// get normalization constant for the total tag normalization
+		double normCONST = sumReads/manager.getSamples().size();
+		
+		ArrayListSorter[] regionCounts = new ArrayListSorter[countRegions.size()];
+		for (int i = 0 ; i < countRegions.size(); i++){
+			regionCounts[i] = new ArrayListSorter();
+			float counts = 0;
+			for (Sample sample : manager.getSamples())
+				counts += (float) (normCONST*sample.countHits(countRegions.get(i))/sample.getHitCount());
+			// add counts
+			regionCounts[i].setVal(counts);		
+			
+			//add peaks or regions
+			if (points !=null)
+				regionCounts[i].setKey(points.get(i));
+			else if (strandedReg != null)
+				regionCounts[i].setKey(strandedReg.get(i));
+			else if (regions != null)
+				regionCounts[i].setKey(regions.get(i));		
+		}	
+		Arrays.sort(regionCounts);
+		
+		// outputting the list of regions in descending order of counts
+		for (int i = 0; i < countRegions.size(); i++)
+			System.out.println(regionCounts[i].getKey());		
+		manager.close();
+	}
+	
+	class ArrayListSorter implements Comparable<ArrayListSorter>{		
+		private Object key;
+		private float val;		
+		public ArrayListSorter(){}		
+		public ArrayListSorter(Object key , float val){
+			super();
+			this.key = key;
+			this.val = val;
+		}		
+		public Object getKey(){
+			return key;
+		}		
+		public float getVal(){ 
+			return val;
+		}
+		public void setKey(Object key){
+			this.key = key;
+		}		
+		public void setVal(float val){
+			this.val = val;
+		}		
+		public int compareTo (ArrayListSorter compareObj){			
+			float compareVal = ((ArrayListSorter) compareObj).getVal();			
+			// descending order
+			if (compareVal > this.val){
+				return 1;
+			}else{
+				return -1;
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		ArgParser ap = new ArgParser(args);		
+        if((!ap.hasKey("peak") && !ap.hasKey("region") && !ap.hasKey("sregion") )) { 
+        	System.err.println("Please provide peak, region, sregion file !");
+            System.err.println("Usage:\n" +
+                               "RegionCountSorter\n" +
+                               "--species <organism;genome> OR\n" +
+                               "--geninfo <genome info file> AND --seq <path to seqs>\n" +
+                               "--peaks <list of peaks> OR --region <list of regions> OR --sregion <list of stranded regions> \n" +
+                               "--expt <experiments> \n" +
+                               "\nOPTIONS:\n" +
+                               "--win <window size around peaks> \n" +
+                               "");
+            System.exit(0);
+        }
+		
+		GenomeConfig gconf = new GenomeConfig(args);
+		ExptConfig econf = new ExptConfig(gconf.getGenome(), args);		
+		econf.setPerBaseReadFiltering(false);		
+		ExperimentManager manager = new ExperimentManager(econf);
+		
+		// parsing command line arguments	
+		int win = Args.parseInteger(args, "win", -1);
+		RegionCountSorter sorter = new RegionCountSorter(gconf,manager,econf,win);		
+		List<Region> reg = new ArrayList<Region>() ;
+		List<StrandedRegion> sreg ;
+		if (ap.hasKey("peak")){
+			List<Point> points = RegionFileUtilities.loadPointsFromFile(ap.getKeyValue("peak"), gconf.getGenome());
+			sorter.setPoint(points);
+		}
+		else if (ap.hasKey("sregion")){
+			sreg = RegionFileUtilities.loadStrandedRegionsFromMotifFile(gconf.getGenome(), ap.getKeyValue("sregion"), -1);
+			sorter.setStrandedRegion(sreg);
+		}else if (ap.hasKey("region")){
+			reg = RegionFileUtilities.loadRegionsFromFile(ap.getKeyValue("region"), gconf.getGenome(), -1);
+			sorter.setRegion(reg);
+		}
+		
+		sorter.execute();
+	}
+
+}
